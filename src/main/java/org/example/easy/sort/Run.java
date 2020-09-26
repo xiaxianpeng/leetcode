23868804b59f95b7b4b80347719519666fa9aadd
夏先鹏 <951256916>
2020-09-26 22:25:16 +0800

    希尔排序

--- src/main/java/org/example/easy/sort/Run.java
+++ src/main/java/org/example/easy/sort/Run.java
@@ -9,16 +9,20 @@ public class Run {
 
 
     public static void main(String[] args) {
-        int[] arr = SortUtil.generateRandomArray(10);
+        int[] arr = SortUtil.generateRandomArray(100);
         int[] arr2 = new int[arr.length];
         System.arraycopy(arr, 0, arr2, 0, arr.length);
 
         // 选择排序
         // SortUtil.check(arr, SelectionSort.sort(arr2));
+
         // 冒泡排序
         //SortUtil.check(arr, BubbleSort.sort(arr2));
-        // 插入 排序
-        SortUtil.check(arr, InsertionSort.sort(arr2));
 
+        // 插入 排序
+        //SortUtil.check(arr, InsertionSort.sort(arr2));
+
+        // 希尔排序
+        SortUtil.check(arr, ShellSort.sort(arr2));
     }
 }
---
+++ src/main/java/org/example/easy/sort/ShellSort.java
@@ -0,0 +1,40 @@
+package org.example.easy.sort;
+
+/**
+ * Created by xianpeng.xia
+ * on 2020/9/26 9:25 下午
+ * 希尔排序，也称递减增量排序算法，是插入排序的一种更高效的改进版本。但希尔排序是非稳定排序算法。
+ *
+ * 先将整个待排序序列分割为若干子序列进行插入排序，使得整个序列基本有序，再进行插入排序
+ */
+public class ShellSort {
+
+    public static int[] sort(int[] arr) {
+        SortUtil.print(arr);
+        // Knuth序列
+        int h = 1;
+        while (h <= arr.length / 3) {
+            h = h * 3 + 1;
+        }
+
+        for (int gap = h; gap > 0; gap = (gap - 1) / 3) {
+
+            for (int i = 1; i < arr.length; i++) {
+
+                for (int j = i; j > gap - 1; j -= gap) {
+                    if (arr[j] < arr[j - gap]) {
+                        SortUtil.swap(arr, j, j - gap);
+                    }
+                    SortUtil.print(arr);
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{9, 6, 11, 3, 5, 12, 8, 7, 10, 15, 14, 4, 1, 13, 2};
+        sort(arr);
+    }
+}
