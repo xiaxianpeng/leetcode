a183ec0cd0a8b78f2cb48aba72acb3e75b880477
夏先鹏 <951256916>
2020-09-26 21:24:02 +0800

    插入排序

---
+++ src/main/java/org/example/easy/sort/InsertionSort.java
@@ -0,0 +1,29 @@
+package org.example.easy.sort;
+
+/**
+ * Created by xianpeng.xia
+ * on 2020/9/26 9:02 下午
+ * 将第一待排序序列的第一个元素看成有序序列，把第二个元素到最后一个元素当成未排序序列
+ * 从头到尾依次扫描未排序序列，将扫描到的每个元素插入到有序序列的适当位置
+ */
+public class InsertionSort {
+
+    public static int[] sort(int[] arr) {
+
+        SortUtil.print(arr);
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    SortUtil.swap(arr, j, j - 1);
+                    SortUtil.print(arr);
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{9, 6, 1, 3, 5};
+        sort(arr);
+    }
+}
--- src/main/java/org/example/easy/sort/Run.java
+++ src/main/java/org/example/easy/sort/Run.java
@@ -9,14 +9,16 @@ public class Run {
 
 
     public static void main(String[] args) {
-        int[] arr = SortUtil.generateRandomArray(5);
+        int[] arr = SortUtil.generateRandomArray(10);
         int[] arr2 = new int[arr.length];
         System.arraycopy(arr, 0, arr2, 0, arr.length);
 
         // 选择排序
         // SortUtil.check(arr, SelectionSort.sort(arr2));
         // 冒泡排序
-        SortUtil.check(arr, BubbleSort.sort(arr2));
+        //SortUtil.check(arr, BubbleSort.sort(arr2));
+        // 插入 排序
+        SortUtil.check(arr, InsertionSort.sort(arr2));
 
     }
 }
