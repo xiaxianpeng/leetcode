b5ca617b2e5db8beecec5f7a56e608e37ea80b64
夏先鹏 <2825615792@xx.com>
2020-11-30 20:55:45 +0800

    单向链表更新和删除节点

--- src/main/java/org/example/linkedList/HeroList.java
+++ src/main/java/org/example/linkedList/HeroList.java
@@ -6,7 +6,7 @@ import org.example.bean.ListNode;
 /**
  * Created by xianpeng.xia
  * on 2020/11/29 下午11:42
- *
+ * <p>
  * 使用单向链表存储水浒108好汉
  */
 public class HeroList extends SingleLinkedList {
@@ -47,6 +47,53 @@ public class HeroList extends SingleLinkedList {
         }
     }
 
+    public void update(ListNode<Hero> hero) {
+        ListNode<Hero> temp = getHead();
+        boolean flag = false;
+        // 找到要更新的节点的上一个节点
+        while (true) {
+            if (temp.getNextNode() == null) {
+                break;
+            }
+            int no = ((Hero) temp.getNextNode().getVal()).getNo();
+            if (no == hero.getVal().getNo()) {
+                flag = true;
+                break;
+            }
+            temp = temp.getNextNode();
+        }
+        if (flag) {
+            ListNode<Hero> node = temp.getNextNode();
+            Hero val = node.getVal();
+            val.setName(hero.getVal().getName());
+            val.setNickname(hero.getVal().getNickname());
+        } else {
+            System.out.printf("未找到%d，无法更新", hero.getVal().getNo());
+        }
+    }
+
+    public void delete(int no) {
+        ListNode<Hero> temp = getHead();
+        boolean flag = false;
+        // 找到要更新的节点的上一个节点
+        while (true) {
+            if (temp.getNextNode() == null) {
+                break;
+            }
+            int currentNo = ((Hero) temp.getNextNode().getVal()).getNo();
+            if (currentNo == no) {
+                flag = true;
+                break;
+            }
+            temp = temp.getNextNode();
+        }
+        if (flag) {
+            temp.setNextNode(temp.getNextNode().getNextNode());
+        } else {
+            System.out.printf("未找到%d，无法删除", no);
+        }
+    }
+
     public static void main(String[] args) {
         Hero hero0 = new Hero(0, "", "");
         Hero hero1 = new Hero(1, "宋江", "及时雨");
@@ -54,6 +101,7 @@ public class HeroList extends SingleLinkedList {
         Hero hero3 = new Hero(3, "吴用", "智多星");
         Hero hero4 = new Hero(4, "公孙胜", "入云龙");
         Hero hero5 = new Hero(5, "关胜", "大刀");
+        Hero hero33 = new Hero(3, "吴用~", "智多星~~");
 
         HeroList heroList = new HeroList(new ListNode<>(hero0));
         heroList.addByNo(new ListNode(hero1));
@@ -61,9 +109,11 @@ public class HeroList extends SingleLinkedList {
         heroList.addByNo(new ListNode(hero5));
         heroList.addByNo(new ListNode(hero2));
         heroList.addByNo(new ListNode(hero4));
-
         heroList.print();
-
+        heroList.update(new ListNode(hero33));
+        heroList.print();
+        heroList.delete(2);
+        heroList.print();
 
     }
 }
