e77e14bb9b32271c25e42e60df28d6dd3cdcd3a4
夏先鹏 <951256916>
2021-01-17 16:55:50 +0800

    生成随机数组工具类

--- src/main/java/org/example/util/ArrayGenerator.java
+++ src/main/java/org/example/util/ArrayGenerator.java
@@ -1,5 +1,7 @@
 package org.example.util;
 
+import java.util.Random;
+
 /**
  * @author xianpeng.xia
  * on 2021/1/17 2:16 下午
@@ -17,4 +19,17 @@ public class ArrayGenerator {
         return arr;
     }
 
+    /**
+     * @param n 数组长度
+     * @param bound 上界
+     * @return 长度为n, 数字范围是[0, bound)的数组
+     */
+    public static Integer[] generateRandomArray(int n, int bound) {
+        Integer[] arr = new Integer[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = random.nextInt(bound);
+        }
+        return arr;
+    }
 }
