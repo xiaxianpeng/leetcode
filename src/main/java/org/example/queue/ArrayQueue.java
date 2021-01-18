c9b63bd0dd2b965631b34a727d137e24f8f6a187
夏先鹏 <951256916>
2021-01-18 22:49:42 +0800

    基于动态数组实现队列

---
+++ src/main/java/org/example/queue/ArrayQueue.java
@@ -0,0 +1,74 @@
+package org.example.queue;
+
+import org.example.array.Array;
+
+/**
+ * @author xianpeng.xia
+ * on 2021/1/18 10:36 下午
+ */
+public class ArrayQueue<E> implements Queue<E> {
+
+    private Array<E> array;
+
+    public ArrayQueue(int capacity) {
+        this.array = new Array<>(capacity);
+    }
+
+    public ArrayQueue() {
+        this.array = new Array<>();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity() {
+        return array.getCapacity();
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("ArrayQueue : ");
+        sb.append("[");
+        for (int i = 0; i < getSize(); i++) {
+            sb.append(array.get(i) + ",");
+        }
+        sb.deleteCharAt(sb.toString().length() - 1);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/queue/Queue.java
@@ -0,0 +1,18 @@
+package org.example.queue;
+
+/**
+ * @author xianpeng.xia
+ * on 2021/1/18 10:34 下午
+ */
+public interface Queue<E> {
+
+    void enqueue(E e);
+
+    E dequeue();
+
+    E getFront();
+
+    int getSize();
+
+    boolean isEmpty();
+}
