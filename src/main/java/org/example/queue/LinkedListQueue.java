4161c3ead718ebeb3f9232f85a75df5dd8249f45
夏先鹏 <951256916>
2021-01-20 23:18:38 +0800

    基于链表实现队列

---
+++ src/main/java/org/example/queue/LinkedListQueue.java
@@ -0,0 +1,114 @@
+package org.example.queue;
+
+/**
+ * @author xianpeng.xia
+ * on 2021/1/20 10:48 下午
+ */
+public class LinkedListQueue<E> implements Queue<E> {
+
+    private class Node {
+
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head, tail;
+    private int size;
+
+    public LinkedListQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    /**
+     * 往tail插入元素
+     */
+    @Override
+    public void enqueue(E e) {
+        if (tail == null) {
+            tail = new Node(e);
+            head = tail;
+        } else {
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    /**
+     * 从head删除元素
+     */
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
+        }
+        Node delNode = head;
+        head = head.next;
+        delNode.next = null;
+        size--;
+        return delNode.e;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty.");
+        }
+        return head.e;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Queue: front ");
+        Node cur = head;
+        while (cur != null) {
+            sb.append(cur + " -> ");
+            cur = cur.next;
+        }
+        sb.append("NULL tail");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
