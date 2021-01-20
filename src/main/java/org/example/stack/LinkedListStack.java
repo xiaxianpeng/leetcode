c955e80d1c130e9818bfa45b4141b90de3c719db
夏先鹏 <2825615792@xx.com>
2021-01-20 19:51:07 +0800

    基于链表实现的栈

---
+++ src/main/java/org/example/stack/LinkedListStack.java
@@ -0,0 +1,61 @@
+package org.example.stack;
+
+import org.example.linkedList.LinkedList;
+
+/**
+ * @date 2021/01/20
+ * @time 19:30
+ */
+public class LinkedListStack<E> implements Stack<E> {
+
+    private LinkedList<E> linkedList;
+
+    public LinkedListStack() {
+        this.linkedList = new LinkedList<>();
+    }
+
+    @Override
+    public void push(E e) {
+        linkedList.addFirst(e);
+    }
+
+    @Override
+    public E pop() {
+        return linkedList.removeFirst();
+    }
+
+    @Override
+    public E peek() {
+        return linkedList.getFirst();
+    }
+
+    @Override
+    public int getSize() {
+        return linkedList.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return linkedList.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Stack: top ");
+        sb.append(linkedList);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack<Integer> stack = new LinkedListStack<>();
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+        stack.pop();
+        System.out.println(stack);
+        stack.peek();
+        System.out.println(stack);
+    }
+}
