7bfd2f73ecf0ee158219d63545de67921643a016
夏先鹏 <951256916>
2021-01-22 22:36:14 +0800

    删除链表中的元素

---
+++ src/main/java/org/example/linkedList/ListNode.java
@@ -0,0 +1,40 @@
+package org.example.linkedList;
+
+/**
+ * @author xianpeng.xia
+ * on 2021/1/22 8:59 下午
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("arr can not be null");
+        }
+        this.val = arr[0];
+        ListNode cur = this;
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        ListNode cur = this;
+        while (cur != null) {
+            sb.append(cur.val + " -> ");
+            cur = cur.next;
+        }
+        sb.append("NULL");
+        return sb.toString();
+    }
+}
