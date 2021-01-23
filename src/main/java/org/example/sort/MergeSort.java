ac38bea654969610941bf4b4c7fd8ff46a614e05
夏先鹏 <2825615792@xx.com>
2021-01-23 17:55:41 +0800

    归并排序基于内存操作优化

--- src/main/java/org/example/sort/MergeSort.java
+++ src/main/java/org/example/sort/MergeSort.java
@@ -67,40 +67,41 @@ public class MergeSort {
     }
 
     public static <E extends Comparable<E>> void sort(E[] arr) {
-        sort(arr, 0, arr.length - 1);
+        E[] temp = Arrays.copyOf(arr, arr.length);
+        sort(arr, 0, arr.length - 1, temp);
     }
 
-    public static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
+    public static <E extends Comparable<E>> void sort(E[] arr, int l, int r, E[] temp) {
         if (l >= r) {
             return;
         }
         int mid = l + (r - l) / 2;
-        sort(arr, l, mid);
-        sort(arr, mid + 1, r);
+        sort(arr, l, mid, temp);
+        sort(arr, mid + 1, r, temp);
         // 有序的就不merge操作了
         if (arr[mid].compareTo(arr[mid + 1]) > 0) {
-            merge(arr, l, mid, r);
+            merge(arr, l, mid, r, temp);
         }
     }
 
     /**
      * 合并两个有序数组arr[l,mid]和arr[mid+1,r]
      */
-    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r) {
-        E[] temp = Arrays.copyOfRange(arr, l, r + 1);
+    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r, E[] temp) {
+        System.arraycopy(arr, l, temp, l, r - l + 1);
         int i = l, j = mid + 1;
         for (int k = l; k <= r; k++) {
             if (i > mid) {
-                arr[k] = temp[j - l];
+                arr[k] = temp[j];
                 j++;
             } else if (j > r) {
-                arr[k] = temp[i - l];
+                arr[k] = temp[i];
                 i++;
-            } else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
-                arr[k] = temp[i - l];
+            } else if (temp[i].compareTo(temp[j]) <= 0) {
+                arr[k] = temp[i];
                 i++;
             } else {
-                arr[k] = temp[j - l];
+                arr[k] = temp[j];
                 j++;
             }
         }
