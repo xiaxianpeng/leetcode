aec24358b402279a968d06da603333ce73fd3a62
夏先鹏 <2825615792@xx.com>
2021-01-23 15:19:34 +0800

    归并排序支持范型

--- src/main/java/org/example/sort/MergeSort.java
+++ src/main/java/org/example/sort/MergeSort.java
@@ -1,12 +1,15 @@
 package org.example.sort;
 
+import java.util.Arrays;
+import org.example.util.ArrayGenerator;
+
 /**
  * Created by xianpeng.xia
  * on 2020/10/1 4:04 下午
- *
+ * <p>
  * 归并排序（Merge sort）是建立在归并操作上的一种有效的排序算法。
  * 该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。'
- *
+ * <p>
  * * 1 申请空间，大小为两个序列之和，以便存放合并后的序列
  * * 2 设置两个指针。最初位置分别为两序列的其实位置
  * * 3 比较两指针所指向的元素，选择相对小的放入合并序列中，并移动指针到下一位置
@@ -14,6 +17,9 @@ package org.example.sort;
  * * 5 将另一序列的剩下的元素复制到合并空间序列尾
  */
 public class MergeSort {
+
+    private MergeSort() {
+    }
 
     public static int[] sort(int[] arr, int left, int right) {
         if (left == right) {
@@ -32,9 +38,9 @@ public class MergeSort {
     /**
      * 保证左右排好序
      *
-     * @param arr 数组
-     * @param leftPtr 左指针
-     * @param rightPtr 右指针
+     * @param arr        数组
+     * @param leftPtr    左指针
+     * @param rightPtr   右指针
      * @param rightBound 右边界
      */
     static void merge(int[] arr, int leftPtr, int rightPtr, int rightBound) {
@@ -60,8 +66,49 @@ public class MergeSort {
         SortUtil.print(arr);
     }
 
-    public static void main(String[] args) {
-        int[] arr = new int[]{1, 4, 7, 8, 3, 6, 9};
+    public static <E extends Comparable<E>> void sort(E[] arr) {
         sort(arr, 0, arr.length - 1);
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        sort(arr, l, mid);
+        sort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    /**
+     * 合并两个有序数组arr[l,mid]和arr[mid+1,r]
+     */
+    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r) {
+        E[] temp = Arrays.copyOfRange(arr, l, r + 1);
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
+                arr[k] = temp[j - l];
+                j++;
+            } else if (j > r) {
+                arr[k] = temp[i - l];
+                i++;
+            } else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
+                arr[k] = temp[i - l];
+                i++;
+            } else {
+                arr[k] = temp[j - l];
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = ArrayGenerator.generateRandomArray(100, 1000);
+        SortUtil.print(arr);
+        MergeSort.sort(arr);
+        boolean sorted = SortUtil.isSorted(arr);
+        System.out.println(sorted);
+        SortUtil.print(arr);
     }
 }
