ffbdcabb5a645c2c7200a93fc4051998ec3c98fa
夏先鹏 <951256916>
2021-01-31 23:09:30 +0800

    format

--- src/main/java/org/example/sort/QuickSort.java
+++ src/main/java/org/example/sort/QuickSort.java
@@ -20,115 +20,6 @@ public class QuickSort {
     private QuickSort() {
     }
 
-    public static int[] sort(int[] arr, int left, int right) {
-        if (left >= right) {
-            return arr;
-        }
-        int index = partition(arr, left, right);
-        sort(arr, left, index - 1);
-        sort(arr, index + 1, right);
-        return arr;
-    }
-
-    /**
-     * @param arr 数组
-     * @param leftBound 左边界 以最左为基准
-     * @param rightBound 右边界
-     * @return 轴的位置
-     */
-    private static int partition(int[] arr, int leftBound, int rightBound) {
-        int pivot = arr[rightBound];
-        int left = leftBound;
-        int right = rightBound - 1;
-        while (left <= right) {
-            SortUtil.print(arr);
-
-            while (left <= right && arr[left] <= pivot) {
-                left++;
-            }
-
-            while (left <= right && arr[right] > pivot) {
-                right--;
-            }
-            //交换左边比pivot大的数,且右边比pivot小的数
-            if (left < right) {
-                SortUtil.swap(arr, left, right);
-            }
-
-            SortUtil.print(arr);
-        }
-        // 将基数放到中间位置
-        SortUtil.swap(arr, left, rightBound);
-        SortUtil.print(arr);
-        return left;
-    }
-
-    private static <E extends Comparable<E>> void sort(E[] arr) {
-        sort(arr, 0, arr.length - 1);
-    }
-
-    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
-        if (l >= r) {
-            return;
-        }
-        int p = partition(arr, l, r);
-        sort(arr, l, p - 1);
-        sort(arr, p + 1, r);
-    }
-
-    private static <E extends Comparable<E>> int partition(E[] arr, int l, int r) {
-        //arr[l+1,j] < v,arr[j+1,i] >= v
-        int j = l;
-        for (int i = l + 1; i <= r; i++) {
-            // 以arr[l]做轴
-            if (arr[i].compareTo(arr[l]) < 0) {
-                j++;
-                SortUtil.swap(arr, i, j);
-            }
-        }
-        SortUtil.swap(arr, l, j);
-        return j;
-    }
-
-    private static <E extends Comparable<E>> void twoWaySort(E[] arr) {
-        twoWaySort(arr, 0, arr.length - 1);
-    }
-
-    private static <E extends Comparable<E>> void twoWaySort(E[] arr, int l, int r) {
-        if (l >= r) {
-            return;
-        }
-        int p = twoWayPartition(arr, l, r);
-        twoWaySort(arr, l, p - 1);
-        twoWaySort(arr, p + 1, r);
-    }
-
-    private static <E extends Comparable<E>> int twoWayPartition(E[] arr, int l, int r) {
-        // 生成[l,r]之间的随机索引
-        int p = new Random().nextInt(r - l + 1) + l;
-        SortUtil.swap(arr, l, p);
-        //arr[l+1,i-1] <= v,arr[j+1,r] >= v
-        int i = l + 1;
-        int j = r;
-
-        while (true) {
-            while (i <= j && arr[i].compareTo(arr[l]) < 0) {
-                i++;
-            }
-            while (j >= i && arr[j].compareTo(arr[l]) > 0) {
-                j--;
-            }
-            if (i >= j) {
-                break;
-            }
-            SortUtil.swap(arr, i, j);
-            i++;
-            j--;
-        }
-        SortUtil.swap(arr, l, j);
-        return j;
-    }
-
     private static <E extends Comparable<E>> void threeWaySort(E[] arr) {
         threeWaySort(arr, 0, arr.length - 1);
     }
@@ -158,7 +49,7 @@ public class QuickSort {
             }
         }
         SortUtil.swap(arr, l, lt);
-        // arr[l,lt-1] > v ,arr[lt,i-1] == v ,arr[gt,r] > v
+        // arr[l,lt-1] > v ,arr[lt,gt-1] == v ,arr[gt,r] > v
 
         threeWaySort(arr, l, lt - 1);
         threeWaySort(arr, gt, r);
