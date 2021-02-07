91a20f0ecbb2a819ac84a6f6a6028863b5c3261f
夏先鹏 <2825615792@xx.com>
2021-02-07 14:55:10 +0800

    二分搜索树 删除最大最小元素

--- src/main/java/org/example/tree/BST.java
+++ src/main/java/org/example/tree/BST.java
@@ -217,6 +217,46 @@ public class BST<E extends Comparable<E>> {
         return maximum(node.right);
     }
 
+    /**
+     * 删除最小元素
+     */
+    public E removeMin() {
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    /**
+     * 删除Node最小元素
+     */
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    public E removeMax() {
+        E max = maximum();
+        root = removeMax(root);
+        return max;
+    }
+
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
     @Override
     public String toString() {
         StringBuffer res = new StringBuffer();
@@ -250,6 +290,11 @@ public class BST<E extends Comparable<E>> {
         }
         System.out.println(bst.minimum());
         System.out.println(bst.maximum());
+        System.out.println("~~~~~~~~~~~~~~~");
+        bst.removeMin();
+        System.out.println("~~~~~~~~~~~~~~~");
+        bst.removeMax();
+        System.out.println(bst);
         /*
         System.out.println(bst.contains(3));
 
