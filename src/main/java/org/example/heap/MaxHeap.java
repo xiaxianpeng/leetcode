c26bf45b617a5e6653aaab4462eabffbbe7c520b
夏先鹏 <2825615792@xx.com>
2021-02-22 16:37:14 +0800

    优先队列 基于堆实现

--- src/main/java/org/example/heap/MaxHeap.java
+++ src/main/java/org/example/heap/MaxHeap.java
@@ -75,7 +75,7 @@ public class MaxHeap<E extends Comparable<E>> {
         }
     }
 
-    private E findMax() {
+    public E findMax() {
         if (size() == 0) {
             throw new IllegalArgumentException("Can not findMax when heep is empty.");
         }
---
+++ src/main/java/org/example/queue/PriorityQueue.java
@@ -0,0 +1,41 @@
+package org.example.queue;
+
+import org.example.heap.MaxHeap;
+
+/**
+ * @date 2021/02/22
+ * @time 16:32
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        this.maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+}
