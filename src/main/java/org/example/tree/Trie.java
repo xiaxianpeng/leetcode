e0eec44e39895b55d74c73d5ed97836923888f66
夏先鹏 <2825615792@xx.com>
2021-03-05 17:11:34 +0800

    字典树 增 查 前缀查询

---
+++ src/main/java/org/example/tree/Trie.java
@@ -0,0 +1,99 @@
+package org.example.tree;
+
+import java.util.TreeMap;
+
+/**
+ * @date 2021/03/05
+ * @time 15:24
+ * 字典树
+ */
+public class Trie {
+
+    private class Node {
+
+        private boolean isWord;
+        private TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            this.next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 向Trie中添加一个新的单词word
+     */
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    /**
+     * 查询单词word是否在Trie中
+     */
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    /**
+     * 查询是否在Trie中有单词以prefix为前缀
+     */
+    public boolean isPrefix(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.add("panda");
+        trie.add("dog");
+        trie.add("cat");
+
+        System.out.println(trie.contains("dog"));
+        System.out.println(trie.contains("pan"));
+        System.out.println(trie.contains("panda"));
+        System.out.println(trie.isPrefix("panx"));
+
+    }
+}
