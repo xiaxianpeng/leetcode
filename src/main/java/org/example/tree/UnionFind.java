6296bde0fd8440176e19f555fa73ed8dddc3f483
夏先鹏 <2825615792@xx.com>
2021-03-06 15:58:56 +0800

    并查集 查找和合并

--- src/main/java/org/example/tree/UnionFind.java
+++ src/main/java/org/example/tree/UnionFind.java
@@ -1,23 +1,31 @@
 package org.example.tree;
 
+import java.util.Random;
+
 /**
  * @date 2021/03/06
  * @time 14:11
  */
 public class UnionFind implements UF {
 
-    private int[] id;
+    private int[] parent;
+    /**
+     * rank[i]表示以i为根的集合所表示的树的层数
+     */
+    private int[] rank;
 
     public UnionFind(int size) {
-        id = new int[size];
-        for (int i = 0; i < id.length; i++) {
-            id[i] = i;
+        parent = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+            rank[i] = 1;
         }
     }
 
     @Override
     public int getSize() {
-        return id.length;
+        return parent.length;
     }
 
     /**
@@ -33,22 +41,59 @@ public class UnionFind implements UF {
      */
     @Override
     public void union(int p, int q) {
-        int pID = find(p);
-        int qID = find(q);
-        if (pID == qID) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
             return;
         }
-        for (int i = 0; i < id.length; i++) {
-            if (id[i] == pID) {
-                id[i] = qID;
-            }
+
+        // 根据两个元素所在树的rank不同判断合并方向
+        // 将rank低的集合合并到rank高的集合上
+
+        if (rank[pRoot] < rank[qRoot]) {
+            parent[pRoot] = qRoot;
+        } else if (rank[qRoot] < rank[pRoot]) {
+            parent[qRoot] = pRoot;
+        } else {//rank[pRoot] == rank[qRoot]
+            parent[qRoot] = pRoot;
+            rank[pRoot] += 1;
         }
     }
 
+    /**
+     * 查找过程,查找元素p所对应的集合编号
+     * O(h)复杂度,h为树的高度
+     */
     private int find(int p) {
-        if (p < 0 || p >= id.length) {
+        if (p < 0 || p >= parent.length) {
             throw new IllegalArgumentException("p is out of bound.");
         }
-        return id[p];
+        while (p != parent[p]) {
+            // 路径压缩
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        int size = 1000;
+        int m = 1000;
+
+        UnionFind unionFind = new UnionFind(size);
+
+        Random random = new Random();
+        for (int i = 0; i < m; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            unionFind.union(a, b);
+        }
+
+        for (int i = 0; i < m; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            unionFind.isConnected(a, b);
+        }
     }
 }
