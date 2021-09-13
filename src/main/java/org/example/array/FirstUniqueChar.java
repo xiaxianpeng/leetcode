e553bb96e6652bcefa3ad15577e8df63b0ddff4e
夏先鹏 <951256916>
2021-09-13 22:27:06 +0800

    字符串中第一个唯一字符出现的索引位置(加注释)

--- src/main/java/org/example/array/FirstUniqueChar.java
+++ src/main/java/org/example/array/FirstUniqueChar.java
@@ -5,14 +5,18 @@ package org.example.array;
  * on 2021/2/28 9:14 下午
  *
  * https://leetcode.com/problems/first-unique-character-in-a-string/
+ *
+ * 字符串中的第一个唯一字符
  */
 public class FirstUniqueChar {
 
     public static int solution(String s) {
         int[] freq = new int[26];
+        // 遍历字符串统计字符频次
         for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
         }
+        // 遍历字符串找到字符频次为1的索引
         for (int i = 0; i < s.length(); i++) {
             if (freq[s.charAt(i) - 'a'] == 1) {
                 return i;
