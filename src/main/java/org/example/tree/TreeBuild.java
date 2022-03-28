52397ff314d7179bbc8f955b8d4a7bc0b65ed5b5
夏先鹏 <951256916>
2022-03-28 21:51:05 +0800

    后序和中序数组构造树

--- src/main/java/org/example/tree/TreeBuild.java
+++ src/main/java/org/example/tree/TreeBuild.java
@@ -44,4 +44,43 @@ public class TreeBuild {
             inorder, index + 1, inEnd);
         return root;
     }
+
+    /**
+     * @param inorder 中序遍历
+     * @param postorder 后序遍历
+     * @return ⼆叉树
+     * 给定⼀棵树的后序遍历结果 postorder 与中序遍历结果 inorder，
+     * 请构造⼆叉树并返回其根节点。
+     */
+    TreeNode buildTree4inAndPost(int[] inorder, int[] postorder) {
+        return build4InAndPost(inorder, 0, inorder.length - 1,
+            postorder, 0, postorder.length - 1);
+    }
+
+    TreeNode build4InAndPost(int[] inorder, int inStart, int inEnd,
+        int[] postorder, int postStart, int postEnd) {
+
+        if (inStart > inEnd) {
+            return null;
+        }
+        // root节点为postorder数组的最后一个元素
+        int rootVal = postorder[postEnd];
+        // rootVal在中序遍历数组的索引
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                index = i;
+                break;
+            }
+        }
+        // 左子树的节点个数
+        int leftSize = index - inStart;
+        TreeNode root = new TreeNode(rootVal);
+        // 递归构造左右子树
+        root.left = build4InAndPost(inorder, inStart, index - 1,
+            postorder, postStart, postStart + leftSize - 1);
+        root.right = build4InAndPost(inorder, index + 1, inEnd,
+            postorder, postStart + leftSize, postEnd - 1);
+        return root;
+    }
 }
