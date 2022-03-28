5f0364b5836f18563ade3399c3a365e31a99f74b
xiapeng <xiapeng@didiglobal.com>
2022-03-28 15:34:33 +0800

    链表两数相加

--- src/main/java/org/example/linkedList/AddTwoNumbers.java
+++ src/main/java/org/example/linkedList/AddTwoNumbers.java
@@ -1,55 +1,51 @@
 package org.example.linkedList;
 
-import org.example.bean.ListNode;
-
 /**
  * @date 2020/09/30
  * @time 16:38
+ * 链表两数相加
  */
 public class AddTwoNumbers {
 
     public static ListNode solution(ListNode l1, ListNode l2) {
+        // 虚拟头节点
         ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
+        // 在两条链表上的指针
+        ListNode p1 = l1, p2 = l2;
+        // 指针p负责构建新链表
+        ListNode p = dummyHead;
         // 进位
         int carry = 0;
-        while (p != null || q != null) {
-            int x = p.getVal() == null ? 0 : (int) p.getVal();
-            int y = q.getVal() == null ? 0 : (int) q.getVal();
-            int sum = x + y + carry;
+        // 执行加法，两条链表走完且没有进位才能结束循环
+        while (p1 != null || p2 != null || carry > 0) {
+            // 先加上进位
+            int val = carry;
+            if (p1 != null) {
+                val += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                val += p2.val;
+                p2 = p2.next;
+            }
             // 进位
-            carry = sum / 10;
+            carry = val / 10;
             // 余数
-            int remainder = sum % 10;
-            curr.setNextNode(new ListNode(remainder));
-            curr = curr.getNextNode();
-            if (p != null) {
-                p = p.getNextNode();
-            }
-            if (q != null) {
-                q = q.getNextNode();
-            }
+            int remainder = val % 10;
+            // 构建新节点
+            p.next = new ListNode(remainder);
+            p = p.next;
         }
-        return dummyHead.getNextNode();
+        return dummyHead.next;
     }
 
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(2);
-        ListNode l2 = new ListNode(4);
-        ListNode l3 = new ListNode(3);
-        l1.setNextNode(l2);
-        l2.setNextNode(l3);
-
-        ListNode l4 = new ListNode(5);
-        ListNode l5 = new ListNode(6);
-        ListNode l6 = new ListNode(4);
-        l4.setNextNode(l5);
-        l5.setNextNode(l6);
-
+        ListNode l1 = new ListNode(new int[]{2, 4, 3});
+        ListNode l2 = new ListNode(new int[]{5, 6, 4});
         // 342 + 465 = 807
         System.out.println(l1);
-        System.out.println(l4);
-        ListNode sum = solution(l1, l4);
+        System.out.println(l2);
+        ListNode sum = solution(l1, l2);
         System.out.println(sum);
 
     }
