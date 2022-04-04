2e77def1022b9dcfbad2715767ad84569606e968
夏先鹏 <951256916>
2022-04-04 16:01:20 +0800

    rename

--- src/main/java/org/example/array/RandomSort.java
+++ src/main/java/org/example/array/RandomSort.java
@@ -25,7 +25,7 @@ public class RandomSort {
     private static void change(int[] original, int[] arr, int target) {
         int size = arr.length;
         int[] indexes = getIndexesWithout(size, target);
-        int middle = arr[target];
+        int temp = arr[target];
 
         Random random = new Random();
 
@@ -38,13 +38,13 @@ public class RandomSort {
         if (arr[index] == original[index]) {
             //与原数组元素相等，未变位置，可以交换
             arr[target] = arr[index];
-            arr[index] = middle;
+            arr[index] = temp;
         } else {
             //若选中的元素是已经更换位置的，则继续尝试与数组中的其它元素交换位置
             for (int i = 0; i < size && i != index && i != target; i++) {
                 if (arr[i] == original[i]) {
                     arr[target] = arr[i];
-                    arr[i] = middle;
+                    arr[i] = temp;
                 }
             }
         }
