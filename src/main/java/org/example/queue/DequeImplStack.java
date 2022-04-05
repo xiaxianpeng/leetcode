cab4e5569b406984caf64c262455a9b8c58f67bb
夏先鹏 <951256916>
2022-04-05 13:41:04 +0800

    队列实现栈(queue,topElem)

---
+++ src/main/java/org/example/queue/DequeImplStack.java
@@ -0,0 +1,54 @@
+package org.example.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/5 1:28 PM
+ * 队列实现栈
+ */
+public class DequeImplStack {
+
+    Queue<Integer> queue = new LinkedList<>();
+    int topElem = 0;
+
+    /**
+     * @param x x
+     * 添加元素到栈顶
+     */
+    public void push(int x) {
+        // x是队列的尾，是栈的顶
+        queue.offer(x);
+        topElem = x;
+    }
+
+    public int top() {
+        return topElem;
+    }
+
+    public int pop() {
+        int size = queue.size();
+        // 留下队尾两个元素
+        while (size > 2) {
+            queue.offer(queue.poll());
+            size--;
+        }
+        topElem = queue.peek();
+        queue.offer(queue.poll());
+        //
+        return queue.poll();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        DequeImplStack stack = new DequeImplStack();
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.pop());
+        System.out.println(stack);
+    }
+}
---
+++ src/main/java/org/example/queue/StackImplQueue.java
@@ -0,0 +1,55 @@
+package org.example.queue;
+
+import java.util.Stack;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/5 11:03 AM
+ * 使用栈实现队列
+ */
+public class StackImplQueue {
+
+    private Stack<Integer> s1, s2;
+
+    public StackImplQueue() {
+        this.s1 = new Stack<>();
+        this.s2 = new Stack<>();
+    }
+
+    /**
+     * push
+     *
+     * @param x 元素
+     */
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    /**
+     * 返回队头元素
+     */
+    public int peek() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+
+    /**
+     * 删除队头的元素并返回
+     */
+    public int pop() {
+        // 先调用peek(),防止s2为空
+        peek();
+        return s2.pop();
+    }
+
+    /**
+     * 判断队列是否为空
+     */
+    public boolean isEmpty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
