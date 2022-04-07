9ab75f33f025651cd13ab01d4d931fd2dd745bda
夏先鹏 <951256916>
2022-04-07 23:05:08 +0800

    排序数组中两个数字之和(双指针)

--- src/main/java/org/example/array/TwoSum.java
+++ src/main/java/org/example/array/TwoSum.java
@@ -1,5 +1,6 @@
 package org.example.array;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,12 +22,39 @@ public class TwoSum {
         return null;
     }
 
+    /**
+     * @param nuns 有序数组
+     * @param target 目标值
+     * @return 元素下标数组
+     *
+     * 初始时两个指针分别指向第一个元素位置和最后一个元素的位置。
+     * 每次计算两个指针指向的两个元素之和，并和目标值比较。
+     * 如果两个元素之和等于目标值，则发现了唯一解。
+     * 如果两个元素之和小于目标值，则将左侧指针右移一位。
+     * 如果两个元素之和大于目标值，则将右侧指针左移一位。
+     * 移动指针之后，重复上述操作，直到找到答案。
+     * 链接：https://leetcode-cn.com/problems/kLl5u1/solution/pai-xu-shu-zu-zhong-liang-ge-shu-zi-zhi-8tv13/
+     */
+    public static int[] twoSumFromSortArray(int[] nuns, int target) {
+        // 前一个元素和后一个元素的指针
+        int low = 0, high = nuns.length - 1;
+        while (low < high) {
+            int sum = nuns[low] + nuns[high];
+            if (sum == target) {
+                return new int[]{low, high};
+            } else if (sum < target) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
-        int[] indexs = solution(nums, target);
-        for (int index : indexs) {
-            System.out.println(index);
-        }
+        int[] indexes = twoSumFromSortArray(nums, target);
+        System.out.println(Arrays.toString(indexes));
     }
 }
