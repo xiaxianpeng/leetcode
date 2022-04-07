91880332191071c333771e769952f5ed6cba68a0
夏先鹏 <951256916>
2022-04-07 21:29:22 +0800

    栈的压入、弹出序列(辅助栈)

---
+++ src/main/java/org/example/stack/ValidateStackSequences.java
@@ -0,0 +1,61 @@
+package org.example.stack;
+
+import java.util.Stack;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/7 8:58 PM
+ * 剑指 Offer 31. 栈的压入、弹出序列
+ * 输入两个整数序列，第一个序列表示栈的压入顺序，
+ * 请判断第二个序列是否为该栈的弹出顺序。
+ * 假设压入栈的所有数字均不相等。
+ * 例如，序列 {1,2,3,4,5} 是某栈的压栈序列，
+ * 序列 {4,5,3,2,1} 是该压栈序列对应的一个弹出序列，
+ * 但 {4,3,5,1,2} 就不可能是该压栈序列的弹出序列。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+ * 输出：true
+ * 解释：我们可以按以下顺序执行：
+ * push(1), push(2), push(3), push(4), pop() -> 4,
+ * push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+ * 示例 2：
+ *
+ * 输入：pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+ * 输出：false
+ * 解释：1 不能在 2 之前弹出。
+ *
+ *
+ * 提示：
+ *
+ * 0 <= pushed.length == popped.length <= 1000
+ * 0 <= pushed[i], popped[i] < 1000
+ * pushed 是 popped 的排列。
+ */
+public class ValidateStackSequences {
+
+    public static boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for (int num : pushed) {
+            // 栈顶入栈
+            stack.push(num);
+            // 循环判断"栈顶元素=弹出序列的当前元素"是否成立，
+            // 符合弹出序列顺序的栈顶元素全部弹出
+            while (!stack.isEmpty() && stack.peek() == popped[i]) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        int[] pushed = new int[]{1, 2, 3, 4, 5};
+        int[] popped = new int[]{4, 5, 3, 2, 1};
+        System.out.println(validateStackSequences(pushed, popped));
+    }
+}
