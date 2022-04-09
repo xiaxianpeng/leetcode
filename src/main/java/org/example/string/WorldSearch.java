84a56db7a807d205a8bfd72fd2880959ea2d5be1
夏先鹏 <951256916>
2022-04-09 21:36:33 +0800

    todo

---
+++ src/main/java/org/example/string/WorldSearch.java
@@ -0,0 +1,30 @@
+package org.example.string;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/9 9:29 PM
+ *
+ * 给定一个m x n 二维字符网格board 和一个字符串单词word 。
+ * 如果word 存在于网格中，返回 true ；否则，返回 false 。
+ *
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，
+ * 其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+ * 同一个单元格内的字母不允许被重复使用。
+ *
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * 输出：true
+ *
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+ * 输出：true
+ *
+ *
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+ * 输出：false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/word-search
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class WorldSearch {
+
+}
