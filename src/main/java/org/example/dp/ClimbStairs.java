dec656ca2f019e4816b0e520910331792737b69f
夏先鹏 <951256916>
2022-04-09 18:49:48 +0800

    爬楼梯(dp)

---
+++ src/main/java/org/example/dp/ClimbStairs.java
@@ -0,0 +1,56 @@
+package org.example.dp;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/9 6:29 PM
+ *
+ * 假设你正在爬楼梯。需要 n阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 2
+ * 输出：2
+ * 解释：有两种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶
+ * 2. 2 阶
+ * 示例 2：
+ *
+ * 输入：n = 3
+ * 输出：3
+ * 解释：有三种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶 + 1 阶
+ * 2. 1 阶 + 2 阶
+ * 3. 2 阶 + 1 阶
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/climbing-stairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ClimbStairs {
+
+    /**
+     * @param n 楼梯的阶数
+     * 本问题其实常规解法可以分成多个子问题，爬第n阶楼梯的方法数量，等于 2 部分之和
+     * 1、爬上 n-1n−1 阶楼梯的方法数量。因为再爬1阶就能到第n阶
+     * 2、爬上 n-2n−2 阶楼梯的方法数量，因为再爬2阶就能到第n阶
+     */
+    public static int climbStairs(int n) {
+        // dp[n] = dp[n-1] + dp[n-2]
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int climbStairs = climbStairs(n);
+        System.out.println(climbStairs);
+    }
+}
