cbce6d12b5feccb519bcd697372c5263b2b48c75
夏先鹏 <951256916>
2022-04-10 16:24:07 +0800

    填充每个节点的下一个右侧节点指针(层序遍历，next=queue.peek)

--- src/main/java/org/example/tree/Connect.java
+++ src/main/java/org/example/tree/Connect.java
@@ -1,5 +1,8 @@
 package org.example.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author xianpeng.xia
  * on 2022/3/28 11:35 PM
@@ -26,12 +29,16 @@ package org.example.tree;
 
 public class Connect {
 
-    private class Node {
+    private static class Node {
 
         int val;
         Node left;
         Node right;
         Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
     }
 
     /**
@@ -48,7 +55,7 @@ public class Connect {
     }
 
     // [三叉树]遍历框架
-    static void traverse(Node leftNode, Node rightNode) {
+    private static void traverse(Node leftNode, Node rightNode) {
         if (leftNode == null || rightNode == null) {
             return;
         }
@@ -62,5 +69,54 @@ public class Connect {
 
         // 连接跨越父节点的两个子节点
         traverse(leftNode.right, rightNode.left);
+    }
+
+    /**
+     * @param root root
+     * 层序遍历
+     */
+    public static Node connectByLevelOrder(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                // 从队首取出元素
+                Node node = queue.poll();
+                // 连接
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                // 拓展下一层节点
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        // 返回root
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Node connect = connectByLevelOrder(root);
+        System.out.printf("");
     }
 }
