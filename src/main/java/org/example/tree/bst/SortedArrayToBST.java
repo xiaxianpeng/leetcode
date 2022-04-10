a4dee348996e046040baed3af3a881d258385a3d
夏先鹏 <951256916>
2022-04-10 16:04:38 +0800

    将有序数组转换为二叉搜索树(dsf)

---
+++ src/main/java/org/example/tree/bst/SortedArrayToBST.java
@@ -0,0 +1,59 @@
+package org.example.tree.bst;
+
+import org.example.tree.TreeNode;
+import org.example.util.TreeUtil;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/10 1:38 PM
+ *
+ * 108. 将有序数组转换为二叉搜索树
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ *
+ * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [-10,-3,0,5,9]
+ * 输出：[0,-3,9,-10,null,5]
+ * 解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,3]
+ * 输出：[3,1]
+ * 解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+ */
+public class SortedArrayToBST {
+
+    private static int x = 100;
+
+    /**
+     * BST的中序遍历是升序的，
+     * 因此本题等同于根据中序遍历的序列恢复二叉搜索树。
+     */
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return dsf(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode dsf(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dsf(nums, lo, mid - 1);
+        root.right = dsf(nums, mid + 1, hi);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-10, -3, 0, 5, 9};
+        TreeNode root = sortedArrayToBST(nums);
+        TreeUtil.print(root);
+    }
+}
