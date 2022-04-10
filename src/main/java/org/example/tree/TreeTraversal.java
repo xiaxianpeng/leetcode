5d73c0fe2ce8e61cf73198cbd72b2e49d1e8a45a
夏先鹏 <951256916>
2022-04-10 11:39:31 +0800

    锯齿形层序遍历(层序+双端队列)

--- src/main/java/org/example/tree/TreeTraversal.java
+++ src/main/java/org/example/tree/TreeTraversal.java
@@ -1,5 +1,7 @@
 package org.example.tree;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -90,5 +92,57 @@ public class TreeTraversal {
             }
         }
         return res;
+    }
+
+    /**
+     * @param root root
+     * @return 锯齿形层序遍历
+     *
+     * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/solution/er-cha-shu-de-ju-chi-xing-ceng-xu-bian-l-qsun/
+     */
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode<Integer> root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        // 默认根节点层数为0，层数为偶数
+        boolean isOrderLeft = true;
+        while (!queue.isEmpty()) {
+            // 双端队列存储每层val
+            Deque<Integer> levelQueue = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode<Integer> curNode = queue.poll();
+                if (isOrderLeft) {//如果从左至右，我们每次将被遍历到的元素插入至双端队列的末尾。
+                    levelQueue.offerLast(curNode.val);
+                } else {//如果从右至左，我们每次将被遍历到的元素插入至双端队列的头部。
+                    levelQueue.offerFirst(curNode.val);
+                }
+
+                if (curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+            }
+            //
+            ans.add(new LinkedList<>(levelQueue));
+            isOrderLeft = !isOrderLeft;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(3);
+        root.left = new TreeNode<>(9);
+        root.right = new TreeNode<>(20);
+        root.right.left = new TreeNode<>(15);
+        root.right.right = new TreeNode<>(7);
+
+        List<List<Integer>> ans = zigzagLevelOrder(root);
+        System.out.println(ans);
     }
 }
