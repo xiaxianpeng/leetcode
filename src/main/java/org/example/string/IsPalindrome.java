552762658234c255877f31a184ea88ddd5559692
夏先鹏 <951256916>
2022-04-10 18:40:21 +0800

    验证回文串(双指针)

---
+++ src/main/java/org/example/string/IsPalindrome.java
@@ -0,0 +1,62 @@
+package org.example.string;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/10 6:34 PM
+ *
+ * 125. 验证回文串
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ *
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * 解释："amanaplanacanalpanama" 是回文串
+ * 示例 2:
+ *
+ * 输入: "race a car"
+ * 输出: false
+ * 解释："raceacar" 不是回文串
+ *
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 2 * 105
+ * 字符串 s 由 ASCII 字符组成
+ */
+public class IsPalindrome {
+
+    /**
+     * 判断是否是回文（双指针）
+     */
+    public static boolean isPalindrome(String s) {
+        // 保存数字或字母的字符
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        //双指针
+        int left = 0, right = sb.length() - 1;
+        while (left < right) {
+            if (sb.charAt(left) != sb.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+}
