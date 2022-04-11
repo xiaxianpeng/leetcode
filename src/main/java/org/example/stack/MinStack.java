ee4842c16744a7129693c902c62b4d692fdc21f0
夏先鹏 <951256916>
2022-04-11 09:28:46 +0800

    rename

--- src/main/java/org/example/stack/MinStack.java
+++ src/main/java/org/example/stack/MinStack.java
@@ -10,27 +10,33 @@ import java.util.LinkedList;
  */
 public class MinStack {
 
-    Deque<Integer> xstack;
+    /**
+     * 数据栈
+     */
+    Deque<Integer> data;
+    /**
+     * 辅助栈
+     */
     Deque<Integer> minStack;
 
     public MinStack() {
-        this.xstack = new LinkedList<>();
+        this.data = new LinkedList<>();
         this.minStack = new LinkedList<>();
         this.minStack.push(Integer.MAX_VALUE);
     }
 
     public void push(int val) {
-        xstack.push(val);
+        data.push(val);
         minStack.push(Math.min(minStack.peek(), val));
     }
 
     public void pop() {
-        xstack.pop();
+        data.pop();
         minStack.pop();
     }
 
     public int top() {
-        return xstack.peek();
+        return data.peek();
     }
 
     public int getMin() {
