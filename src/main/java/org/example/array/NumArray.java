21b1f1841552247e6adc602aad5dfa571bdec9c7
夏先鹏 <951256916>
2022-04-14 17:27:58 +0800

    前缀和问题

--- src/main/java/org/example/array/NumArray.java
+++ src/main/java/org/example/array/NumArray.java
@@ -13,19 +13,25 @@ public class NumArray {
     /**
      * sum[i]存储前i个元素和,sun[i]存储nums[0,...i-1]的和,sum[0] = 0,
      */
-    private int[] sum;
+    private int[] preSum;
 
+    /**
+     * /* 输入一个数组，构造前缀和
+     */
     public NumArray(int[] nums) {
-        sum = new int[nums.length + 1];
-
-        sum[0] = 0;
-        for (int i = 1; i < sum.length; i++) {
-            sum[i] = sum[i - 1] + nums[i - 1];
+        preSum = new int[nums.length + 1];
+        // preSum[0] = 0，便于计算累加和
+        preSum[0] = 0;
+        for (int i = 1; i < preSum.length; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
         }
     }
 
-    public int sumRange(int i, int j) {
-        return sum[j + 1] - sum[i];
+    /**
+     * 查询闭区间 [left, right] 的累加和
+     */
+    public int sumRange(int left, int right) {
+        return preSum[right + 1] - preSum[left];
     }
 
     public static void main(String[] args) {
