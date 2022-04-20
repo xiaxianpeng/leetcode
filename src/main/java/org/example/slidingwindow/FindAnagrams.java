47a5e0880d3c73b598273ebf3acb4b3740e1e844
夏先鹏 <951256916>
2022-04-20 20:26:21 +0800

    字符串p在s中的异位词的子串的索引(滑动窗口)

---
+++ src/main/java/org/example/slidingwindow/FindAnagrams.java
@@ -0,0 +1,91 @@
+package org.example.slidingwindow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/20 7:06 PM
+ *
+ * 438. 找到字符串中所有字母异位词
+ * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，
+ * 返回这些子串的起始索引。不考虑答案输出的顺序。
+ *
+ * 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: s = "cbaebabacd", p = "abc"
+ * 输出: [0,6]
+ * 解释:
+ * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+ * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+ * 示例 2:
+ *
+ * 输入: s = "abab", p = "ab"
+ * 输出: [0,1,2]
+ * 解释:
+ * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+ * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+ * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+ */
+public class FindAnagrams {
+
+    /**
+     * @param s s
+     * @param p p
+     *
+     * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，
+     * 返回这些子串的起始索引
+     */
+    public static List<Integer> findAnagrams(String s, String p) {
+        Map<Character, Integer> window = new HashMap<>();
+        Map<Character, Integer> need = new HashMap<>();
+
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int valid = 0;
+        List<Integer> ans = new ArrayList<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            while (right - left == p.length()) {
+                if (valid == need.size()) {
+                    ans.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.getOrDefault(d, 0) - 1);
+                }
+            }
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd", p = "abc";
+        List<Integer> anagramIndex = findAnagrams(s, p);
+        System.out.println(anagramIndex);
+    }
+}
