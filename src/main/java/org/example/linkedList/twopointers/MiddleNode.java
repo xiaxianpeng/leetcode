c25aa4f67c41104f12c68903fd2e3ac7e859c818
夏先鹏 <951256916>
2022-04-21 22:05:21 +0800

    链表的中间结点(快慢指针)

---
+++ src/main/java/org/example/linkedList/twopointers/MiddleNode.java
@@ -0,0 +1,52 @@
+package org.example.linkedList.twopointers;
+
+import org.example.linkedList.ListNode;
+
+/**
+ * @author xianpeng.xia
+ * on 2022/4/21 10:00 PM
+ *
+ * 876. 链表的中间结点
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+ * 示例 2：
+ *
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ */
+public class MiddleNode {
+
+    /**
+     * @param head head
+     * @return 中间节点
+     * 快慢指针返回链表中间节点
+     */
+    public static ListNode middleNode(ListNode head) {
+        // 快慢指针初始化指向 head
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            // 慢指针走一步，快指针走两步
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(new int[]{1, 2, 3, 4, 5});
+        ListNode middleNode = middleNode(head);
+        System.out.println(middleNode);
+    }
+}
