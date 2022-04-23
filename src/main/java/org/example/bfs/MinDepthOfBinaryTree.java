5553079762eab4b89ced829bf81bfe82e6508017
夏先鹏 <951256916>
2022-04-23 22:21:31 +0800

    test

--- src/main/java/org/example/bfs/MinDepthOfBinaryTree.java
+++ src/main/java/org/example/bfs/MinDepthOfBinaryTree.java
@@ -62,4 +62,15 @@ public class MinDepthOfBinaryTree {
         }
         return depth;
     }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        int minDepth = minDepth(root);
+        System.out.println(minDepth);
+    }
 }
