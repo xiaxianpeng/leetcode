eb1cff3d33c9b23f20ab9464365e1f03aa28400c
xiapeng <xiaxianpeng@autohome.com.cn>
2024-10-31 16:29:13 +0800

    栈在括号匹配中的应用

--- src/main/java/org/example/stack/ValidParentheses.java
+++ src/main/java/org/example/stack/ValidParentheses.java
@@ -7,37 +7,27 @@ import org.example.stack.structure.ArrayStack;
  * @time 16:22
  * 题目: https://leetcode-cn.com/problems/valid-parentheses/
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串，判断字符串是否有效。
- * <p>
  * 有效字符串需满足：
- * <p>
  * 左括号必须用相同类型的右括号闭合。
  * 左括号必须以正确的顺序闭合。
  * 注意空字符串可被认为是有效字符串。
- * <p>
  * 示例 1:
  * <p>
  * 输入: "()"
  * 输出: true
- * 示例 2:
- * <p>
+ * 示例 2:
  * 输入: "()[]{}"
  * 输出: true
- * 示例 3:
- * <p>
+ * 示例3:
  * 输入: "(]"
  * 输出: false
- * 示例 4:
- * <p>
+ * 示例 4:
  * 输入: "([)]"
  * 输出: false
- * 示例 5:
- * <p>
+ * 示例 5:
  * 输入: "{[]}"
  * 输出: true
- * <p>
- * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/valid-parentheses
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class ValidParentheses {
 
