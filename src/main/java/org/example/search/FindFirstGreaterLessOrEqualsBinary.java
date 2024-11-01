2099bb5334a8b13fc4c9228b3ce7e01a965fc303
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-01 14:09:36 +0800

    戴拿优化

--- src/main/java/org/example/search/FindFirstGreaterLessOrEqualsBinary.java
+++ src/main/java/org/example/search/FindFirstGreaterLessOrEqualsBinary.java
@@ -41,14 +41,14 @@ public class FindFirstGreaterLessOrEqualsBinary {
         int high = arr.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (arr[mid] > val) {
-                high = mid - 1;
-            } else {
+            if (arr[mid] <= val) {
                 if (mid == arr.length - 1 || arr[mid + 1] > val) {
                     return mid;
                 } else {
                     low = mid + 1;
                 }
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
