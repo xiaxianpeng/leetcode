5192f00b95b2e3cc5a4b6e53521030e9072d267a
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-05 15:47:51 +0800

    三数之和：双指针

--- src/main/java/org/example/array/nsum/ThreeSum.java
+++ src/main/java/org/example/array/nsum/ThreeSum.java
@@ -23,31 +23,38 @@ import java.util.List;
 public class ThreeSum {
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> lists = new ArrayList<>();
-        // 先排序
+        // 1、先将 nums 排序，时间复杂度为 O(NlogN)。
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
         // 双指针
         int len = nums.length;
 
         for (int i = 0; i < len; i++) {
-            // 因为已经排序好，所以后面不可能有三个数加和等于0
+            // 当 nums[i] > 0 时直接break跳出：因为 nums[R] >= nums[L] >= nums[i] > 0，
+            // 即 3 个元素都大于 0 ，在此固定指针 i 之后不可能再找到结果了。
             if (nums[i] > 0) {
-                return lists;
+                break;
             }
             // 避免重复
+            // 当 i > 0且nums[i] == nums[i - 1]时即跳过此元素nums[i]：
+            // 因为已经将 nums[i - 1] 的所有组合加入到结果中，本次双指针搜索只会得到重复组合。
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int curr = nums[i];
+            // L，R 分设在数组索引 (i,len(nums)) 两端，当L < R时循环计算sum = nums[i] + nums[L] + nums[R]，
+            // 并按照以下规则执行双指针移动：
+            //  当sum < 0时，L += 1并跳过所有重复的nums[L]；
+            //  当sum > 0时，R -= 1并跳过所有重复的nums[R]；
+            //  当sum == 0时，记录组合[k, L, R]至res，执行L += 1和R -= 1并跳过所有重复的nums[L]和nums[R]，防止记录到重复组合。
             int L = i + 1, R = len - 1;
             while (L < R) {
-                int temp = curr + nums[L] + nums[R];
-                if (temp == 0) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
                     List<Integer> list = new ArrayList<>();
-                    list.add(curr);
+                    list.add(nums[i]);
                     list.add(nums[L]);
                     list.add(nums[R]);
-                    lists.add(list);
+                    ans.add(list);
                     // 相同数过滤
                     while (L < R && nums[L + 1] == nums[L]) {
                         L++;
@@ -57,17 +64,17 @@ public class ThreeSum {
                     }
                     L++;
                     R--;
-                    // temp<0，说明nums[L]过小
-                } else if (temp < 0) {
+                } else if (sum < 0) {
+                    // sum<0，说明nums[L]过小
                     L++;
-                    // temp>0，说明nums[R]过大
                 } else {
+                    // sum>0，说明nums[R]过大
                     R--;
                 }
             }
         }
 
-        return lists;
+        return ans;
     }
 
     public static void main(String[] args) {
