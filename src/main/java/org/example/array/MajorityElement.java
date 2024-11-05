8119134fb2a79f5a078c0c9331e075390cbed276
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-05 15:29:33 +0800

    摩尔投票：求众数

--- src/main/java/org/example/array/MajorityElement.java
+++ src/main/java/org/example/array/MajorityElement.java
@@ -6,20 +6,20 @@ import java.util.List;
 /**
  * @author xianpeng.xia
  * on 2022/4/11 10:46 AM
- *
+ * <p>
  * 169. 多数元素
  * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
- *
+ * <p>
  * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：[3,2,3]
  * 输出：3
  * 示例 2：
- *
+ * <p>
  * 输入：[2,2,1,1,1,2,2]
  * 输出：2
  */
@@ -32,40 +32,44 @@ public class MajorityElement {
      * @return 出现次数 大于 ⌊ n/2 ⌋ 的元素
      */
     public static int majorityElement(int[] nums) {
-        // 候选人candNum初始化为nums[0], 票数count = 1;
-        int candNum = nums[0], count = 1;
+        // 1、初始化： 票数统计 votes = 0 ， 众数 x。
+        // 众数
+        int x = 0;
+        // 票数
+        int votes = 0;
         for (int num : nums) {
-            if (candNum == num) {
-                count++;
-            } else if (count-- == 0) {
-                candNum = num;
-                count = 1;
+            // 2、当 票数 votes 等于 0 ，则假设当前数字 num 是众数。
+            if (votes == 0) {
+                x = num;
+                // 当 num = x 时，票数 votes 自增 1 ；当 num != x 时，票数 votes 自减 1 。
+                votes += num == x ? 1 : -1;
             }
         }
-        return candNum;
+        //  返回众数x
+        return x;
     }
 
     /**
      * @param nums nums
      * @return 出现次数 大于 ⌊ n/3 ⌋ 的元素
-     *
+     * <p>
      * 229. 求众数 II
      * 给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
-     *
-     *
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
+     * <p>
+     * <p>
      * 示例 1：
-     *
+     * <p>
      * 输入：[3,2,3]
      * 输出：[3]
      * 示例 2：
-     *
+     * <p>
      * 输入：nums = [1]
      * 输出：[1]
      * 示例 3：
-     *
+     * <p>
      * 输入：[1,1,1,3,3,2,2,2]
      * 输出：[1,2]
      */
