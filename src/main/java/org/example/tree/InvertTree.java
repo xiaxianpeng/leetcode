c5458868ebd9d27ea7825433415aa34a0922b83e
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-07 19:10:24 +0800

    翻转二叉树:递归

--- src/main/java/org/example/tree/InvertTree.java
+++ src/main/java/org/example/tree/InvertTree.java
@@ -3,8 +3,20 @@ package org.example.tree;
 /**
  * @author xianpeng.xia
  * on 2022/3/28 11:56 PM
- *
- * 如何翻转⼆叉树？其实就是把⼆叉树上的每个节点的左右⼦节点都交换⼀下
+ * <p>
+ * 226. 翻转二叉树
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ * <p>
+ * 方法一：递归法
+ * 链接：https://leetcode.cn/problems/invert-binary-tree/solutions/2361621/226-fan-zhuan-er-cha-shu-fen-zhi-qing-xi-tqlf/
+ * 根据二叉树镜像的定义，考虑递归遍历（dfs）二叉树，交换每个节点的左 / 右子节点，即可生成二叉树的镜像。
+ * 递归解析：
+ * 终止条件： 当节点 root 为空时（即越过叶节点），则返回 null 。
+ * 递推工作：
+ * 初始化节点 tmp ，用于暂存 root 的左子节点。
+ * 开启递归 右子节点 invertTree(root.right) ，并将返回值作为 root 的 左子节点 。
+ * 开启递归 左子节点 invertTree(tmp) ，并将返回值作为 root 的 右子节点 。
+ * 返回值： 返回当前节点 root 。
  */
 public class InvertTree {
 
@@ -46,5 +58,38 @@ public class InvertTree {
         root.right = left;
         // 和定义逻辑⾃恰：以 root 为根的这棵⼆叉树已经被翻转，返回 root
         return root;
+    }
+
+    /**
+     * * 根据二叉树镜像的定义，考虑递归遍历（dfs）二叉树，交换每个节点的左 / 右子节点，即可生成二叉树的镜像。
+     * * 递归解析：
+     * * 终止条件： 当节点 root 为空时（即越过叶节点），则返回 null 。
+     * * 递推工作：
+     * * 初始化节点 tmp ，用于暂存 root 的左子节点。
+     * * 开启递归 右子节点 invertTree(root.right) ，并将返回值作为 root 的 左子节点 。
+     * * 开启递归 左子节点 invertTree(tmp) ，并将返回值作为 root 的 右子节点 。
+     * * 返回值： 返回当前节点 root 。
+     */
+    public static TreeNode invert(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+        TreeNode left = node.left;
+        node.left = invert(node.right);
+        node.right = invert(left);
+        return node;
+    }
+
+    public static void main(String[] args) {
+
+        // 构建一个示例二叉树
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        TreeUtil.printTree(root);
+
+        TreeNode invert = invert(root);
+        TreeUtil.printTree(invert);
+
     }
 }
---
+++ src/main/java/org/example/tree/TreeUtil.java
@@ -0,0 +1,85 @@
+package org.example.tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TreeUtil {
+
+    // 私有构造函数，防止实例化工具类
+    private TreeUtil() {
+    }
+
+    public static void printTree(TreeNode root) {
+        int maxLevel = maxLevel(root);
+        printNode(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static void printNode(List<TreeNode> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int edgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        printWhitespaces(firstSpaces);
+
+        List<TreeNode> newNodes = new ArrayList<>();
+        for (TreeNode node : nodes) {
+            if (node != null) {
+                System.out.print(node.val);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            printWhitespaces(betweenSpaces);
+        }
+        System.out.println();
+
+        for (int i = 1; i <= edgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    printWhitespaces(edgeLines + edgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null) System.out.print("/");
+                else printWhitespaces(1);
+
+                printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null) System.out.print("\\");
+                else printWhitespaces(1);
+
+                printWhitespaces(edgeLines + edgeLines - i);
+            }
+
+            System.out.println();
+        }
+
+        printNode(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++) System.out.print(" ");
+    }
+
+    private static int maxLevel(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+    }
+
+    private static boolean isAllElementsNull(List<TreeNode> list) {
+        for (TreeNode node : list) {
+            if (node != null) return false;
+        }
+        return true;
+    }
+}
