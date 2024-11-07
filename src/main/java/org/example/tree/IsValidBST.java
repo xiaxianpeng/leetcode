7e6933b92afaa1815267f8fcbe571342c13a6869
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-07 18:01:01 +0800

    验证二叉查找树:递归

--- src/main/java/org/example/tree/IsValidBST.java
+++ src/main/java/org/example/tree/IsValidBST.java
@@ -3,37 +3,43 @@ package org.example.tree;
 /**
  * @author xianpeng.xia
  * on 2022/3/29 9:41 AM
- *
+ * <p>
  * 给你⼀个⼆叉树的根节点 root，判断其是否是⼀个有效的⼆叉搜索树。
  */
 public class IsValidBST<Integer> {
 
+    /**
+     * 是否是二叉搜索树
+     *
+     * @param root root
+     * @return 是否是二叉搜索树
+     */
     public static boolean isValidBST(TreeNode root) {
         return isValidBST(root, null, null);
     }
 
     /**
-     * @param root root
-     * @param min 最小值
-     * @param max 最大值
+     * @param node 当前正在检查的节点。
+     * @param min  当前子树允许的最小值节点，如果没有最小值限制则为 null。
+     * @param max  当前子树允许的最大值节点，如果没有最大值限制则为 null。
      * @return 是否是bst
      * 限定以 root 为根的⼦树节点必须满⾜ max.val > root.val > min.val
      */
-    private static boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
-        // base case
-        if (root == null) {
+    private static boolean isValidBST(TreeNode node, TreeNode min, TreeNode max) {
+        // 基准情况：空树是有效的二叉搜索树
+        if (node == null) {
+            return true;
+        }
+        // 每个节点的左子树只包含小于该节点的值，
+        if (min != null && node.val <= min.val) {
             return false;
         }
-        // 若 root.val 不符合 max 和 min 的限制，说明不是合法 BST
-        if (min != null && (int) root.val <= (int) min.val) {
+        // 每个节点的右子树只包含大于该节点的值。
+        if (max != null && node.val >= max.val) {
             return false;
         }
-        if (max != null && (int) root.val >= (int) max.val) {
-            return false;
-        }
-        // 限定左⼦树的最⼤值是 root.val，右⼦树的最⼩值是 root.val
-        return isValidBST(root.left, min, root)
-            && isValidBST(root.right, root, max);
+        // 递归检查左右子树，并更新最小/最大值
+        return isValidBST(node.left, min, node) && isValidBST(node.right, node, max);
     }
 
     /**
@@ -59,5 +65,19 @@ public class IsValidBST<Integer> {
         pre = (int) root.val;
         // 访问右子树
         return isValid(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        System.out.println(isValidBST(root));
+
+        TreeNode invalidRoot = new TreeNode(5);
+        invalidRoot.left = new TreeNode(1);
+        invalidRoot.right = new TreeNode(4);
+        invalidRoot.right.left = new TreeNode(3);
+        invalidRoot.right.right = new TreeNode(6);
+        System.out.println(isValidBST(invalidRoot));
     }
 }
