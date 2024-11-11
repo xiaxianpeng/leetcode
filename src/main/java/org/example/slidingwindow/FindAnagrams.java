d9a0325fc20e5a2706af8ce180a9f805f6b2b611
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-11 10:26:51 +0800

    找到字符串中所有字母异位词:滑动窗口

--- src/main/java/org/example/slidingwindow/FindAnagrams.java
+++ src/main/java/org/example/slidingwindow/FindAnagrams.java
@@ -6,39 +6,31 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author xianpeng.xia
- * on 2022/4/20 7:06 PM
- *
  * 438. 找到字符串中所有字母异位词
  * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，
  * 返回这些子串的起始索引。不考虑答案输出的顺序。
- *
  * 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
- *
- *
- *
  * 示例 1:
- *
  * 输入: s = "cbaebabacd", p = "abc"
  * 输出: [0,6]
  * 解释:
  * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
  * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
  * 示例 2:
- *
  * 输入: s = "abab", p = "ab"
  * 输出: [0,1,2]
  * 解释:
  * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
  * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
  * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+ * https://leetcode.cn/problems/find-all-anagrams-in-a-string/description/?envType=study-plan-v2&envId=labuladong-algorithm-note
  */
 public class FindAnagrams {
 
     /**
-     * @param s s
-     * @param p p
-     *
+     * @param s 主字符串
+     * @param p 字符串 p，其异位词子串为目标
+     * @return 含有所有起始索引的列表
      * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，
      * 返回这些子串的起始索引
      */
@@ -50,7 +42,8 @@ public class FindAnagrams {
             need.put(c, need.getOrDefault(c, 0) + 1);
         }
 
-        int left = 0, right = 0;
+        int left = 0;
+        int right = 0;
         int valid = 0;
         List<Integer> ans = new ArrayList<>();
 
@@ -84,8 +77,7 @@ public class FindAnagrams {
     }
 
     public static void main(String[] args) {
-        String s = "cbaebabacd", p = "abc";
-        List<Integer> anagramIndex = findAnagrams(s, p);
-        System.out.println(anagramIndex);
+        System.out.println(findAnagrams("cbaebabacd", "abc"));
+        System.out.println(findAnagrams("abab", "ab"));
     }
 }
