50ccdc42cfd4e2dbeeaaa68432ed98bf3c377180
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-14 20:09:16 +0800

    两数之和：numsMap(val,index)

--- src/main/java/org/example/array/nsum/TwoSum.java
+++ src/main/java/org/example/array/nsum/TwoSum.java
@@ -5,12 +5,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @date 2020/09/30
- * @time 15:46
+ * 1. 两数之和
+ * 给定一个整数数组 nums 和一个整数目标值 target，
+ * 请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+ * 你可以按任意顺序返回答案。
+ * 示例 1：
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * https://leetcode.cn/problems/two-sum/description/
  */
 public class TwoSum {
 
-    public static int[] solution(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> numsMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int sub = target - nums[i];
@@ -22,39 +36,10 @@ public class TwoSum {
         return null;
     }
 
-    /**
-     * @param nuns 有序数组
-     * @param target 目标值
-     * @return 元素下标数组
-     *
-     * 初始时两个指针分别指向第一个元素位置和最后一个元素的位置。
-     * 每次计算两个指针指向的两个元素之和，并和目标值比较。
-     * 如果两个元素之和等于目标值，则发现了唯一解。
-     * 如果两个元素之和小于目标值，则将左侧指针右移一位。
-     * 如果两个元素之和大于目标值，则将右侧指针左移一位。
-     * 移动指针之后，重复上述操作，直到找到答案。
-     * 链接：https://leetcode-cn.com/problems/kLl5u1/solution/pai-xu-shu-zu-zhong-liang-ge-shu-zi-zhi-8tv13/
-     */
-    public static int[] twoSumFromSortArray(int[] nuns, int target) {
-        // 前一个元素和后一个元素的指针
-        int low = 0, high = nuns.length - 1;
-        while (low < high) {
-            int sum = nuns[low] + nuns[high];
-            if (sum == target) {
-                return new int[]{low, high};
-            } else if (sum < target) {
-                low++;
-            } else {
-                high--;
-            }
-        }
-        return new int[]{-1, -1};
-    }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
-        int[] indexes = twoSumFromSortArray(nums, target);
-        System.out.println(Arrays.toString(indexes));
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));  // 输出示例 1 结果
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 3, 4}, 6)));       // 输出示例 2 结果
+        System.out.println(Arrays.toString(twoSum(new int[]{-1, 0}, -1)));       // 输出示例 3 结果
     }
 }
---
+++ src/main/java/org/example/array/nsum/TwoSumFromSortArray.java
@@ -0,0 +1,7 @@
+package org.example.array.nsum;
+
+/**
+ * Created on 2024/11/14 20:02
+ */
+public class TwoSumFromSortArray {
+}
