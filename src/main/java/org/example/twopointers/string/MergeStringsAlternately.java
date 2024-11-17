c49172b0f9ee24eeb888ffe184b49a608ee5fefc
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-17 22:39:11 +0800

    交替合并字符串:双指针依次取字符,拼接剩余部分

---
+++ src/main/java/org/example/twopointers/string/MergeStringsAlternately.java
@@ -0,0 +1,56 @@
+package org.example.twopointers.string;
+
+/**
+ * 1768. 交替合并字符串
+ * 给你两个字符串 word1 和 word2 。
+ * 请你从 word1 开始，通过交替添加字母来合并字符串。
+ * 如果一个字符串比另一个字符串长，就将多出来的字母追加到合并后字符串的末尾。
+ * 返回 合并后的字符串 。
+ * 示例 1：
+ * 输入：word1 = "abc", word2 = "pqr"
+ * 输出："apbqcr"
+ * 解释：字符串合并情况如下所示：
+ * word1：  a   b   c
+ * word2：    p   q   r
+ * 合并后：  a p b q c r
+ * 示例 2：
+ * 输入：word1 = "ab", word2 = "pqrs"
+ * 输出："apbqrs"
+ * 解释：注意，word2 比 word1 长，"rs" 需要追加到合并后字符串的末尾。
+ * word1：  a   b
+ * word2：    p   q   r   s
+ * 合并后：  a p b q   r   s
+ * 示例 3：
+ * 输入：word1 = "abcd", word2 = "pq"
+ * 输出："apbqcd"
+ * 解释：注意，word1 比 word2 长，"cd" 需要追加到合并后字符串的末尾。
+ * word1：  a   b   c   d
+ * word2：    p   q
+ * 合并后：  a p b q c   d
+ * Created on 2024/11/17 22:32
+ */
+public class MergeStringsAlternately {
+
+    public static String mergeAlternately(String word1, String word2) {
+        // 定义两个指针
+        int i = 0;
+        int j = 0;
+        StringBuffer sb = new StringBuffer();
+        // 交替拼接两个字符串
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                sb.append(word1.charAt(i++));
+            }
+            if (j < word2.length()) {
+                sb.append(word2.charAt(j++));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("abc", "pqr")); // 示例 1 输出: "apbqcr"
+        System.out.println(mergeAlternately("ab", "pqrs")); // 示例 2 输出: "apbqrs"
+        System.out.println(mergeAlternately("abcd", "pq")); // 示例 3 输出: "apbqcd"
+    }
+}
