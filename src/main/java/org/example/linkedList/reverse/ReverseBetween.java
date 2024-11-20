73ff4237e361ef546b03ff896eaa8433923d878c
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-20 12:16:27 +0800

    反转链表II:dummy;prev->left-1;curr,next,prev

--- src/main/java/org/example/linkedList/reverse/ReverseBetween.java
+++ src/main/java/org/example/linkedList/reverse/ReverseBetween.java
@@ -3,11 +3,9 @@ package org.example.linkedList.reverse;
 import org.example.linkedList.structure.ListNode;
 
 /**
- * @author xianpeng.xia
- * on 2022/4/22 12:43 AM
- * <p>
  * 92. 反转链表 II
- * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。
+ * 请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
  * 示例 1：
  * 输入：head = [1,2,3,4,5], left = 2, right = 4
  * 输出：[1,4,3,2,5]
@@ -29,14 +27,18 @@ public class ReverseBetween {
             prev = prev.next;
         }
         // 开始节点就是 prev 的下一个节点
-        ListNode start = prev.next;
-        // 借助一个指针来帮助我们在反转过程中移动节点
-        ListNode next = null;
+        ListNode curr = prev.next;
         // 开始进行反转操作
         for (int i = 0; i < right - left; i++) {
-            next = start.next;
-            start.next = next.next;
+            // next 指向当前节点 curr 的下一个节点
+            ListNode next = curr.next;
+            // 将当前节点 curr 的下一个指向 next 的下一个节点，
+            // 实际上将 next 从链表中暂时拿出
+            curr.next = next.next;
+            // 将 next 插入到已经反转的部分的前面，
+            // prev.next 是反转部分的头节点
             next.next = prev.next;
+            // 更新 prev，将 next 节点移动到前面，成为新的头节点
             prev.next = next;
         }
         // 返回反转后的链表的头节点
