634624d60d83dc307fe946f57dc946c982aee355
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-22 16:51:22 +0800

    合并两个有序链表:p=dummy,p1,p2;比较大小;注意null边界

--- src/main/java/org/example/twopointers/linkedlist/MergeTwoSortedLists.java
+++ src/main/java/org/example/twopointers/linkedlist/MergeTwoSortedLists.java
@@ -3,8 +3,6 @@ package org.example.twopointers.linkedlist;
 import org.example.linkedlist.structure.ListNode;
 
 /**
- * @author xianpeng.xia
- * on 2022/4/21 7:28 PM
  * 21. 合并两个有序链表
  * 将两个升序链表合并为一个新的 升序 链表并返回。
  * 新链表是通过拼接给定的两个链表的所有节点组成的。
@@ -17,6 +15,13 @@ import org.example.linkedlist.structure.ListNode;
  */
 public class MergeTwoSortedLists {
 
+    /**
+     * 合并两个有序链表
+     *
+     * @param L1 第一个有序链表的头节点
+     * @param L2 第二个有序链表的头节点
+     * @return 合并后的有序链表的头节点
+     */
     public static ListNode mergeTwoLists(ListNode L1, ListNode L2) {
         // 虚拟头节点
         ListNode dummy = new ListNode(-1);
@@ -25,12 +30,13 @@ public class MergeTwoSortedLists {
         ListNode p2 = L2;
         while (p1 != null && p2 != null) {
             // 比较 p1 和 p2 两个指针
-            if (p1.val > p2.val) {
-                p.next = p2;
-                p2 = p2.next;
-            } else {
+            // 升序
+            if (p1.val <= p2.val) {
                 p.next = p1;
                 p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
             }
             // p 指针不断前进
             p = p.next;
