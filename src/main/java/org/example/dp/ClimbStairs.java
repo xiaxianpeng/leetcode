8bf0be2bc77f262c85aab5f05604fa41808d7901
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-25 23:17:44 +0800

    爬楼梯:dp:{dp[1]=1,dp[2]=2;fori-n{dp[i]=dp[i-1]+dp[i-2]}}

--- src/main/java/org/example/dp/ClimbStairs.java
+++ src/main/java/org/example/dp/ClimbStairs.java
@@ -1,56 +1,55 @@
 package org.example.dp;
 
 /**
- * @author xianpeng.xia
- * on 2022/4/9 6:29 PM
- *
- * 假设你正在爬楼梯。需要 n阶你才能到达楼顶。
- *
+ * 70. 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
- *  
- *
  * 示例 1：
- *
  * 输入：n = 2
  * 输出：2
  * 解释：有两种方法可以爬到楼顶。
  * 1. 1 阶 + 1 阶
  * 2. 2 阶
  * 示例 2：
- *
  * 输入：n = 3
  * 输出：3
  * 解释：有三种方法可以爬到楼顶。
  * 1. 1 阶 + 1 阶 + 1 阶
  * 2. 1 阶 + 2 阶
  * 3. 2 阶 + 1 阶
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/climbing-stairs
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class ClimbStairs {
 
     /**
+     * 计算爬到楼顶的方法总数。
+     * 核心思路：使用动态规划。
+     * 定义 dp[i] 为爬到第 i 阶的方法总数。
+     * 转移方程为 dp[i] = dp[i-1] + dp[i-2]，
+     * 因爬到第 i 阶可以从第 i-1 阶（爬 1 阶）或 i-2 阶（爬 2 阶）达到。
+     *
      * @param n 楼梯的阶数
-     * 本问题其实常规解法可以分成多个子问题，爬第n阶楼梯的方法数量，等于 2 部分之和
-     * 1、爬上 n-1n−1 阶楼梯的方法数量。因为再爬1阶就能到第n阶
-     * 2、爬上 n-2n−2 阶楼梯的方法数量，因为再爬2阶就能到第n阶
+     * @return 不同的方法数
      */
     public static int climbStairs(int n) {
+        // 创建一个数组 dp，其中 dp[i] 表示爬到第 i 阶的方法数
         // dp[n] = dp[n-1] + dp[n-2]
         int[] dp = new int[n + 1];
+
+        // 初始条件：到第一阶只有一种方法，到第二阶有两种方法
         dp[1] = 1;
         dp[2] = 2;
+
+        // 从第三阶开始，应用状态转移方程计算每阶的方法数
         for (int i = 3; i <= n; i++) {
+            // 爬到第 i 阶的方法数等于爬到第 i-1 阶的方法数加上爬到第 i-2 阶的方法数
             dp[i] = dp[i - 1] + dp[i - 2];
         }
+        // 返回爬到第 n 阶的方法数
         return dp[n];
     }
 
     public static void main(String[] args) {
-        int n = 4;
-        int climbStairs = climbStairs(n);
-        System.out.println(climbStairs);
+        System.out.println("Total ways to climb 2 steps: " + climbStairs(2)); // 输出: 2
+        System.out.println("Total ways to climb 3 steps: " + climbStairs(3)); // 输出: 3
     }
 }
