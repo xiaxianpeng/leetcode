e3b8b6d8fb3bb43b68655a56fa0d6b8cd9ee9e96
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-25 16:01:43 +0800

    搜索插入位置:二分查找{left,right;left<=right,mid,left?,right?}

--- src/main/java/org/example/array/binarysearch/SearchInsertPosition.java
+++ src/main/java/org/example/array/binarysearch/SearchInsertPosition.java
@@ -1,57 +1,45 @@
-package org.example.search;
+package org.example.array.binarysearch;
 
 
 /**
- * @author xianpeng.xia
- * on 2022/5/7 00:17
- * https://leetcode-cn.com/problems/search-insert-position/
- *
  * 35. 搜索插入位置
  * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
- *
  * 请必须使用时间复杂度为 O(log n) 的算法。
- *
- *
- *
  * 示例 1:
- *
  * 输入: nums = [1,3,5,6], target = 5
  * 输出: 2
  * 示例 2:
- *
  * 输入: nums = [1,3,5,6], target = 2
  * 输出: 1
  * 示例 3:
- *
  * 输入: nums = [1,3,5,6], target = 7
  * 输出: 4
  */
 public class SearchInsertPosition {
 
     /**
-     * @param nums nums
-     * @param target target
-     * @return target在nums中的位置
+     * 使用二分查找找到目标值在数组中的位置或插入点
+     *
+     * @param nums   排序数组
+     * @param target 目标值
+     * @return 目标值在数组中的位置
      */
-    public int searchInsertPosition(int[] nums, int target) {
+    public static int searchInsert(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2;// 防止溢出
             if (nums[mid] == target) {
-                return mid;
+                return mid;// 找到目标值，返回索引
             } else if (nums[mid] < target) {
-                left = mid + 1;
+                left = mid + 1;// 在右半部分搜索
             } else {
-                right = mid - 1;
+                right = mid - 1;// 在左半部分搜索
             }
         }
-        return left;
+        return left;// 返回插入位置
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 3, 5, 6};
-        int target = 5;
-        int index = new SearchInsertPosition().searchInsertPosition(nums, target);
-        System.out.println(index);
+        System.out.println("index:" + searchInsert(new int[]{1, 3, 5, 6}, 5));
     }
 }
