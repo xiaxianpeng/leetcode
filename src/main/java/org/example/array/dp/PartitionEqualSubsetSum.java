154f8d9332556f9a2df1f8f44ea3461eaa9d15a5
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-27 01:02:49 +0800

    0-1背包问题:<dp>:dp[j]=max(dp[j],dp[j-weight(i)]+values[i])

--- src/main/java/org/example/array/dp/PartitionEqualSubsetSum.java
+++ src/main/java/org/example/array/dp/PartitionEqualSubsetSum.java
@@ -3,6 +3,7 @@ package org.example.array.dp;
 import java.util.Arrays;
 
 /**
+ * ***子集背包问题***
  * 416. 分割等和子集
  * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
  * 示例 1：
--- src/main/java/org/example/dp/array/Knapsack.java
+++ src/main/java/org/example/dp/array/Knapsack.java
@@ -1,55 +1,41 @@
 package org.example.dp.array;
 
 /**
- * 0-1背包
+ * 0-1背包问题
+ * 给你一个可装载重量为 W 的背包和 N 个物品，每个物品有重量和价值两个属性。
+ * 其中第 i 个物品的重量为 wt[i]，价值为 val[i]。
+ * 现在让你用这个背包装物品，每个物品只能用一次，在不超过被包容量的前提下，最多能装的价值是多少？
  */
 public class Knapsack {
-    public static int knapsack(int[] weight, int n, int w) {
-        boolean[][] states = new boolean[n][w + 1]; // 默认值 false
-        states[0][0] = true;  // 第一行的数据要特殊处理，可以利用哨兵优化
-        states[0][weight[0]] = true;
-        for (int i = 1; i < n; ++i) { // 动态规划状态转移
-            for (int j = 0; j <= w; ++j) {// 不把第 i 个物品放入背包
-                if (states[i - 1][j] == true) {
-                    states[i][j] = states[i - 1][j];
-                }
-            }
-            for (int j = 0; j <= w - weight[i]; ++j) {// 把第 i 个物品放入背包
-                if (states[i - 1][j] == true) {
-                    states[i][j + weight[i]] = true;
-                }
-            }
-        }
-        for (int i = w; i >= 0; --i) { // 输出结果
-            if (states[n - 1][i] == true) {
-                return i;
+    /**
+     * 解决思路：使用动态规划来解决0-1背包问题。我们使用一个一维数组 dp，其中 dp[i] 表示背包容量为 i 时可以获得的最大价值。
+     * 在每次选择物品时，我们要倒着更新 dp 数组，确保每个物品只被选择一次。
+     *
+     * @param weights  物品的重量
+     * @param values   物品的价值
+     * @param num      物品的数量
+     * @param capacity 背包的最大容量
+     * @return 背包能够装载的最大价值
+     */
+    public static int knapsack(int[] weights, int[] values, int num, int capacity) {
+        // dp[i] 表示背包容量为 i 时最大可获得的价值
+        int[] dp = new int[capacity + 1];
+        for (int i = 0; i < num; i++) {
+            // 遍历背包容量，从大到小更新 dp 数组
+            for (int j = capacity; j >= weights[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weights[i]] + values[i]);
             }
         }
-        return 0;
-    }
 
-    public static int knapsack2(int[] items, int n, int w) {
-        boolean[] states = new boolean[w + 1]; // 默认值 false
-        states[0] = true;  // 第一行的数据要特殊处理，可以利用哨兵优化
-        states[items[0]] = true;
-        for (int i = 1; i < n; ++i) { // 动态规划
-            for (int j = w - items[i]; j >= 0; --j) {// 把第 i 个物品放入背包
-                if (states[j] == true) {
-                    states[j + items[i]] = true;
-                }
-            }
-        }
-        for (int i = w; i >= 0; --i) { // 输出结果
-            if (states[i] == true) {
-                return i;
-            }
-        }
-        return 0;
+        // 返回背包容量为 capacity 时的最大价值
+        return dp[capacity];
     }
 
     public static void main(String[] args) {
-        int[] weight = {2, 2, 4, 6, 3};
-        System.out.println(knapsack(weight, 5, 9));
-        System.out.println(knapsack2(weight, 5, 9));
+        int num = 3;
+        int capacity = 4;
+        int[] weights = {2, 1, 3};
+        int[] values = {4, 2, 3};
+        System.out.println(knapsack(weights, values, num, capacity));  // 输出: 6
     }
 }
