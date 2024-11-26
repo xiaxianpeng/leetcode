79bb2c6e7b332ffdde147d9af4da7b81902db082
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-26 11:55:06 +0800

    加油站:<贪心>:fori-n{totalGas,totalCost;fori-n{cur=(start+steps)%n;totalGas+=gas[cur],totalCost+=cost[cur];totalCost>totalGas?break} totalGas>=totalCost 返回start}

--- src/main/java/org/example/array/CanCompleteCircuit.java
+++ src/main/java/org/example/array/CanCompleteCircuit.java
@@ -3,18 +3,18 @@ package org.example.array;
 /**
  * @author xianpeng.xia
  * on 2022/4/10 10:17 PM
- *
+ * <p>
  * 134. 加油站
  * 在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
- *
+ * <p>
  * 你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
- *
+ * <p>
  * 给定两个整数数组 gas 和 cost ，如果你可以绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。如果存在解，则 保证 它是 唯一 的。
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
  * 输出: 3
  * 解释:
@@ -26,7 +26,6 @@ package org.example.array;
  * 开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
  * 因此，3 可为起始索引。
  * 示例 2:
- *
  * 输入: gas = [2,3,4], cost = [3,4,3]
  * 输出: -1
  * 解释:
@@ -39,39 +38,55 @@ package org.example.array;
  */
 public class CanCompleteCircuit {
 
+    /**
+     * 解决思路：利用贪心算法，遍历每个加油站作为起点，尝试从该加油站出发，计算能否完成一圈。
+     * 如果在某次尝试中发现油量不足以到达下一个加油站，则从失败点之后的加油站重新尝试。
+     * 因为如果从某个起点出发无法到达某个加油站，那么任何位于这两个加油站之间的站点作为起点也无法到达目标加油站。
+     *
+     * @param gas  每个加油站的汽油量
+     * @param cost 从一个加油站到下一个加油站所需的油量
+     * @return 能够完成一圈的起点加油站的索引，若不存在则返回 -1
+     */
     public static int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
-        int i = 0;
 
-        while (i < n) {
+        // 遍历每一个加油站作为起点
+        for (int startStation = 0; startStation < n; startStation++) {
+            int totalGas = 0;   // 累积油量
+            int totalCost = 0;  // 累积消耗油量
 
-            int sumOfGas = 0, sumOfCost = 0;
-            int cnt = 0;
+            // 从 startStation 出发，尝试行驶一圈
+            for (int steps = 0; steps < n; steps++) {
+                // 当前加油站的位置，循环环绕
+                int currentStation = (startStation + steps) % n;
 
-            while (cnt < n) {
-                int j = (i + cnt) % n;
-                sumOfGas += gas[j];
-                sumOfCost += cost[i];
-                if (sumOfCost > sumOfGas) {
+                // 累加油量和消耗油量
+                totalGas += gas[currentStation];
+                totalCost += cost[currentStation];
+
+                // 如果油量不足以到达下一个加油站，则无法完成一圈
+                if (totalCost > totalGas) {
                     break;
                 }
-                cnt++;
             }
 
-            if (cnt == n) {
-                return i;
-            } else {
-                i = i + cnt + 1;
+            // 如果成功绕一圈，返回当前起始加油站的索引
+            if (totalGas >= totalCost) {
+                return startStation;
             }
-
         }
+
+        // 如果无法从任何一个加油站出发完成一圈，返回 -1
         return -1;
     }
 
     public static void main(String[] args) {
         int[] gas = new int[]{1, 2, 3, 4, 5};
         int[] cost = new int[]{3, 4, 5, 1, 2};
-        int start = canCompleteCircuit(gas, cost);
-        System.out.println(start);
+        System.out.println(canCompleteCircuit(gas, cost));  // 输出: 3
+
+        int[] gas2 = new int[]{2, 3, 4};
+        int[] cost2 = new int[]{3, 4, 3};
+        System.out.println(canCompleteCircuit(gas2, cost2));  // 输出: -1
     }
 }
