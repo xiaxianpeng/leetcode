bd4e7938b3e533d1dabd9532af560aa6c4d46e32
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-27 11:56:39 +0800

    多数元素:<摩尔投票>:candidate,count; count==0{candidate=num,count += (num==candidate)?}

--- src/main/java/org/example/array/MajorityElement.java
+++ src/main/java/org/example/array/MajorityElement.java
@@ -8,42 +8,45 @@ package org.example.array;
  * 输入：[3,2,3]
  * 输出：3
  * 示例 2：
- * <p>
  * 输入：[2,2,1,1,1,2,2]
  * 输出：2
  */
 public class MajorityElement {
 
     /**
-     * 摩尔投票法思路
+     * 使用Boyer-Moore 投票算法找到多数元素
+     * 思路：多数元素出现的次数大于其他元素总和，因此可以通过投票
+     * 算法来确定。每次从数组中取出一个候选，抵消一个非候选，最后
+     * 剩下的就是多数元素。
      *
-     * @param nums nums
-     * @return 出现次数 大于 ⌊ n/2 ⌋ 的元素
+     * @param nums 输入数组
+     * @return 数组中的多数元素
      */
     public static int majorityElement(int[] nums) {
-        // 1、初始化： 票数统计 votes = 0 ， 众数 x。
-        // 众数
-        int x = 0;
-        // 票数
-        int votes = 0;
+        // 初始化候选元素和计数器
+        int candidate = nums[0];
+        int count = 0;
+
+        // 遍历数组
         for (int num : nums) {
-            // 2、当 票数 votes 等于 0 ，则假设当前数字 num 是众数。
-            if (votes == 0) {
-                x = num;
-                // 当 num = x 时，票数 votes 自增 1 ；当 num != x 时，票数 votes 自减 1 。
-                votes += num == x ? 1 : -1;
+            // 如果计数器为0，选择新的候选元素
+            if (count == 0) {
+                candidate = num;
+                // 如果当前元素等于候选元素，增加计数器，否则减少
+                count += (num == candidate) ? 1 : -1;
+                System.out.println("Current number: " + num + ", Candidate: " + candidate + ", Count: " + count);
             }
         }
-        //  返回众数x
-        return x;
+        //  返回多数元素
+        return candidate;
     }
 
 
     public static void main(String[] args) {
+        int[] nums1 = {3, 2, 3};
+        System.out.println("Result for nums1: " + majorityElement(nums1)); // 输出3
 
-        int[] nums = new int[]{2, 2, 1, 1, 1, 2, 2};
-        int majorityElement = majorityElement(nums);
-        System.out.println(majorityElement);
-
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println("Result for nums2: " + majorityElement(nums2)); // 输出2
     }
 }
