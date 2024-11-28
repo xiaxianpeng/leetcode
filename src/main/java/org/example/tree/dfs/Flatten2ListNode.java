d16c0f435a69cc27867d34860ea10e8da71dad4a
xiapeng <xiaxianpeng@autohome.com.cn>
2024-11-28 16:51:11 +0800

    二叉树展开为链表:<dfs>:flatten(node){null,return;暂存left,right;node.right=left;curr=node...right;curr.right=right;flatten(node.right)}

--- src/main/java/org/example/tree/dfs/Flatten2ListNode.java
+++ src/main/java/org/example/tree/dfs/Flatten2ListNode.java
@@ -22,10 +22,10 @@ import org.example.util.TreeUtil;
 public class Flatten2ListNode {
 
     /**
-     * 114. 二叉树展开为链表
-     * 给定二叉树的根结点 root ，请你将它展开为一个单链表：
-     * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
-     * 展开后的单链表应该与二叉树先序遍历顺序相同。
+     * 方法：递归展开二叉树为单链表
+     * 思路：使用前序遍历，将每个节点的左子树插入其右子树位置。
+     *
+     * @param node 二叉树的根节点
      */
     public static void flatten(TreeNode node) {
         // base case：如果当前节点为空，直接返回
@@ -33,11 +33,9 @@ public class Flatten2ListNode {
             return;
         }
 
-        // 递归展开左子树和右子树
-        flatten(node.left);
-        flatten(node.right);
+        System.out.println("Flattening node with value: " + node.val);
 
-        // 1、暂存左子树
+        // 1、暂存左右子树
         TreeNode left = node.left;
         TreeNode right = node.right;
 
@@ -45,25 +43,42 @@ public class Flatten2ListNode {
         node.right = left;
         node.left = null;
 
-        // 3、寻找到右子树的最末端并连接原右子树
+        // 3、找到新的右子树的最后一个节点并连接原右子树
         TreeNode curr = node;
         while (curr.right != null) {
             curr = curr.right;
         }
+
+        // 4、将之前保存的右子树连接到新的右子树末端
         curr.right = right;
 
+        flatten(node.right);
     }
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(5);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(4);
-        root.right.right = new TreeNode(6);
-        TreeUtil.printTree(root);
-        System.out.println(PreorderTraversal.preOrderTraversal(root));
-        flatten(root);
-        TreeUtil.printTree(root);
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(5);
+        root1.left.left = new TreeNode(3);
+        root1.left.right = new TreeNode(4);
+        root1.right.right = new TreeNode(6);
+        TreeUtil.printTree(root1);
+        System.out.println(PreorderTraversal.preOrderTraversal(root1));
+        flatten(root1);
+        TreeUtil.printTree(root1);
+
+
+        TreeNode root2 = null;
+        TreeUtil.printTree(root2);
+        System.out.println(PreorderTraversal.preOrderTraversal(root2));
+        flatten(root2);
+        TreeUtil.printTree(root2);
+
+
+        TreeNode root3 = new TreeNode(0);
+        TreeUtil.printTree(root3);
+        System.out.println(PreorderTraversal.preOrderTraversal(root3));
+        flatten(root3);
+        TreeUtil.printTree(root3);
     }
 }
