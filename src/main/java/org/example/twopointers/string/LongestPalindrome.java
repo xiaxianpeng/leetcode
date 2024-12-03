f32ecf6de4b2744fe027e82000af639aa15893da
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-03 18:13:03 +0800

    最长回文子串<中心扩展法>:fori-len{ palindrome(s,i,i);palindrome(s,i,i+1) }

--- src/main/java/org/example/twopointers/string/LongestPalindrome.java
+++ src/main/java/org/example/twopointers/string/LongestPalindrome.java
@@ -16,14 +16,29 @@ package org.example.twopointers.string;
  */
 public class LongestPalindrome {
 
+    /**
+     * 寻找最长回文子串
+     * 核心思路：使用中心扩展法，以每个字符或相邻字符对为中心向两边扩展，检查回文子串的长度。
+     * 比较每个中心扩展的回文子串，记录最长的回文子串。
+     *
+     * @param s 输入的字符串
+     * @return 最长回文子串
+     */
     public static String longestPalindrome(String s) {
+        // 判断特殊情况 // 如果输入为空字符串，直接返回
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+
         // 用于保存找到的最长回文子串
         String ans = "";
+        // 遍历每个字符，以它为中心扩展
         for (int i = 0; i < s.length(); i++) {
-            // 以字符 s[i] 为中心的最长回文子串
-            String s1 = palindrome(s, i, i);
-            // 以字符 s[i] 和 s[i+1] 为中心的最长回文子串，适用于偶数长度的回文
-            String s2 = palindrome(s, i, i + 1);
+            // 扩展回文的中心，i是回文的中心，分为两种情况：
+            // 1. 回文中心是单个字符：s[i]
+            // 2. 回文中心是两个相同字符：s[i] 和 s[i + 1]
+            String s1 = palindrome(s, i, i); // 以s[i]为中心
+            String s2 = palindrome(s, i, i + 1);// 以s[i]和s[i + 1]为中心
             // 更新 ans 为最长的回文子串
             ans = ans.length() > s1.length() ? ans : s1;
             ans = ans.length() > s2.length() ? ans : s2;
@@ -32,13 +47,22 @@ public class LongestPalindrome {
         return ans;
     }
 
+    /**
+     * 从给定的中心扩展回文子串
+     *
+     * @param s     输入的字符串
+     * @param left  左边的起始位置
+     * @param right 右边的起始位置
+     * @return 从中心扩展得到的回文子串的长度
+     */
     public static String palindrome(String s, int left, int right) {
         // 当 L 和 R 指向的字符相同且没有越界时，向两边扩展
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        // 因为跳出循环时，L 和 R 指向的字符不同，所以实际的回文串是从 L+1 到 R-1 位置的子串
+        // 扩展结束时，返回的回文子串的长度
         return s.substring(left + 1, right);
     }
 
