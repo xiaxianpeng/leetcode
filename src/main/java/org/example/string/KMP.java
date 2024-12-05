e6146aee20a938b2c6c752f74548de6b2172bc8e
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-05 10:20:28 +0800

    找出字符串中第一个匹配项的下标:<KMP>

--- src/main/java/org/example/string/KMP.java
+++ src/main/java/org/example/string/KMP.java
@@ -1,42 +1,20 @@
 package org.example.string;
 
 /**
- * @author xianpeng.xia
- * on 2022/4/8 9:45 PM
- *
- * 实现strStr()函数。
- *
- * 给你两个字符串haystack 和 needle ，
- * 请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回-1 。
- *
- *
- * 说明：
- *
- * 当needle是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
- *
- * 对于本题而言，当needle是空字符串时我们应当返回 0 。这与 C 语言的strstr()以及 Java 的indexOf()定义相符。
- *
- *
- *
+ * 28. 找出字符串中第一个匹配项的下标
+ * 给你两个字符串 haystack 和 needle ，
+ * 请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
+ * 如果 needle 不是 haystack 的一部分，则返回  -1 。
  * 示例 1：
- *
- * 输入：haystack = "hello", needle = "ll"
- * 输出：2
- * 示例 2：
- *
- * 输入：haystack = "aaaaa", needle = "bba"
- * 输出：-1
- * 示例 3：
- *
- * 输入：haystack = "", needle = ""
+ * 输入：haystack = "sadbutsad", needle = "sad"
  * 输出：0
- *  
- *
- *
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/implement-strstr
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 解释："sad" 在下标 0 和 6 处匹配。
+ * 第一个匹配项的下标是 0 ，所以返回 0 。
+ * 示例 2：
+ * 输入：haystack = "leetcode", needle = "leeto"
+ * 输出：-1
+ * 解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1
+ * Created on 2024/12/5 10:08
  */
 public class KMP {
 
