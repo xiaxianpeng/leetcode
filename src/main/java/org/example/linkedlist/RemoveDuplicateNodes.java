8e3db800155dfdfa52b7ea8444aa08349ca21da2
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-06 15:19:43 +0800

    移除未排序链表中的重复节点:<Set,遍历>: visited,curr;遍历visited.contains(curr.next.val)?

--- src/main/java/org/example/linkedlist/RemoveDuplicateNodes.java
+++ src/main/java/org/example/linkedlist/RemoveDuplicateNodes.java
@@ -6,32 +6,45 @@ import java.util.Set;
 import org.example.linkedlist.structure.ListNode;
 
 /**
- * @author xianpeng.xia
- * on 2022/4/5 8:12 PM
- *
- * 删除未排序链表重复节点
- * https://leetcode-cn.com/problems/remove-duplicate-node-lcci/solution/mian-shi-ti-0201-yi-chu-zhong-fu-jie-dia-u619/
+ * 面试题 02.01. 移除重复节点
+ * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+ * 示例1：
+ * 输入：[1, 2, 3, 3, 2, 1]
+ * 输出：[1, 2, 3]
+ * 示例2：
+ * 输入：[1, 1, 1, 1, 2]
+ * 输出：[1, 2]
+ * 提示：
+ * 链表长度在[0, 20000]范围内。
+ * 链表元素在[0, 20000]范围内。
  */
 public class RemoveDuplicateNodes {
 
+    /**
+     * 删除无序链表中的重复节点，保留首次出现的节点。
+     * 使用一个 HashSet 来记录已访问的节点值，避免重复。
+     *
+     * @param head 链表头节点
+     * @return 返回去重后的链表头节点
+     */
     public static ListNode removeDuplicateNodes(ListNode head) {
         if (head == null) {
-            return head;
+            return null;
         }
+        Set<Integer> visited = new HashSet<>();
+        ListNode curr = head;
+        visited.add(curr.val);// 将头节点的值加入集合
 
-        // 存储已经存在的节点
-        Set<Integer> occurred = new HashSet<>();
-        ListNode pre = null, cur = head;
-        while (cur != null) {
-            // 当前待删除节点
-            if (occurred.contains(cur.val)) {
-                // 删除
-                pre.next = cur.next;
+        // 遍历链表
+        while (curr != null && curr.next != null) {
+            if (visited.contains(curr.next.val)) {
+                // 如果当前节点的下一个节点值已经出现过，跳过它
+                curr.next = curr.next.next;
             } else {
-                occurred.add(cur.val);
-                pre = cur;
+                // 否则，将当前节点的下一个节点值加入集合，并继续移动
+                visited.add(curr.next.val);
+                curr = curr.next;
             }
-            cur = cur.next;
         }
         return head;
     }
@@ -42,5 +55,10 @@ public class RemoveDuplicateNodes {
         System.out.println(head);
         ListNode ans = removeDuplicateNodes(head);
         System.out.println(ans);
+
+        ListNode head2 = new ListNode(new int[]{1, 1, 1, 1, 2});
+        System.out.println(head2);
+        ListNode ans2 = removeDuplicateNodes(head2);
+        System.out.println(ans2);
     }
 }
