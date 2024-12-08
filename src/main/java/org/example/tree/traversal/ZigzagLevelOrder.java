854cf11b6f5686d7a70dc5154ca526f1cb3e74ed
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-08 11:01:35 +0800

    二叉树的锯齿形层序遍历:<队列+双端队列>:queue,isOrderLeft;{Deque<Integer> levelQueue}

--- src/main/java/org/example/tree/traversal/ZigzagLevelOrder.java
+++ src/main/java/org/example/tree/traversal/ZigzagLevelOrder.java
@@ -26,31 +26,47 @@ import org.example.tree.TreeNode;
 public class ZigzagLevelOrder {
 
     /**
-     * @param root root
-     * @return 锯齿形层序遍历
-     * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/solution/er-cha-shu-de-ju-chi-xing-ceng-xu-bian-l-qsun/
+     * 使用队列实现二叉树的锯齿形层序遍历。
+     * 算法思路：
+     * 1. 使用队列进行层序遍历，每层记录节点值到双端队列中（Deque）。
+     * - 如果当前层从左到右遍历，节点值加入队列尾部。
+     * - 如果当前层从右到左遍历，节点值加入队列头部。
+     * 2. 遍历完当前层后，将双端队列的结果转为普通列表，加入最终结果。
+     * 3. 层级切换时，翻转遍历方向标志 `isOrderLeft`。
+     * 时间复杂度：O(n)，其中 n 是树中节点的数量，每个节点被访问一次。
+     * 空间复杂度：O(n)，队列中最多可能存储所有叶子节点。
+     *
+     * @param root 二叉树的根节点
+     * @return 锯齿形层序遍历的节点值列表
      */
     public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> zigzagLevelList = new ArrayList<>();
         if (root == null) {
-            return ans;// 如果根节点为 null，直接返回空列表
+            return zigzagLevelList; // 空树返回空列表
         }
+
+        // 使用队列进行层序遍历
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
-        boolean isOrderLeft = true;// 控制遍历方向，初始为从左到右
+        boolean isOrderLeft = true;// 遍历方向标志，初始为从左到右
+
         while (!queue.isEmpty()) {
-            // 双端队列存储每层val
-            Deque<Integer> levelQueue = new LinkedList<>();
-            int size = queue.size();
+            Deque<Integer> levelQueue = new LinkedList<>();// 双端队列存储当前层节点值
+            int size = queue.size();// 当前层的节点数
+
             for (int i = 0; i < size; i++) {
+                // 取出队列头部的节点
                 TreeNode curNode = queue.poll();
-                if (isOrderLeft) {//如果从左至右，我们每次将被遍历到的元素插入至双端队列的末尾。
+
+                // 根据遍历方向将节点值加入双端队列
+                if (isOrderLeft) {// 如果从左到右：加入队列尾部
                     levelQueue.offerLast(curNode.val);
-                } else {//如果从右至左，我们每次将被遍历到的元素插入至双端队列的头部。
+                } else {// 如果从右到左：加入队列头部
                     levelQueue.offerFirst(curNode.val);
                 }
 
+                // 将左右子节点加入队列
                 if (curNode.left != null) {
                     queue.offer(curNode.left);
                 }
@@ -58,11 +74,13 @@ public class ZigzagLevelOrder {
                     queue.offer(curNode.right);
                 }
             }
-            //
-            ans.add(new LinkedList<>(levelQueue));// 当前层遍历结果加入最终答案
-            isOrderLeft = !isOrderLeft;// 切换方向
+
+            // 将当前层结果加入最终结果
+            zigzagLevelList.add(new LinkedList<>(levelQueue));
+            // 切换遍历方向
+            isOrderLeft = !isOrderLeft;
         }
-        return ans;
+        return zigzagLevelList;
     }
 
 
