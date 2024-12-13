fea42dce6634c02d59069b0811543e578ac87b0f
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-13 17:10:55 +0800

    在排序数组中查找元素的第一个和最后一个位置:<二分查找>:left<=right;找左边界{nums[mid]<target,left=mid+1;right=mid-1} ; 找右边界{nums[mid]>target,right=mid-1;left=mid+1}

--- src/main/java/org/example/array/binarysearch/SearchRange.java
+++ src/main/java/org/example/array/binarysearch/SearchRange.java
@@ -1,63 +1,85 @@
 package org.example.array.binarysearch;
 
-import org.example.sort.SortUtil;
+import java.util.Arrays;
 
 /**
  * 34. 在排序数组中查找元素的第一个和最后一个位置
- * 给定⼀个按照升序排列的整数数组 nums，和⼀个⽬标值 target，找出给定⽬标值在数组中的开始位置和结束位置。
- * 如果数组中不存在⽬标值 target，返回 [-1, -1]。
- * 示例 1：输⼊：nums = [5,7,7,8,8,10], target = 8
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ * 示例 1：
+ * 输入：nums = [5,7,7,8,8,10], target = 8
  * 输出：[3,4]
- * 示例 2： 输⼊：nums = [5,7,7,8,8,10], target = 6
+ * 示例 2：
+ * 输入：nums = [5,7,7,8,8,10], target = 6
  * 输出：[-1,-1]
- * 基本思路 PS：这道题在《算法⼩抄》 的第 71 ⻚。
- * https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=study-plan-v2&envId=labuladong-algorithm-note
- * ⼆分搜索的难点就在于如何搜索左侧边界和右侧边界，代码的边界的控制⾮常考验你的微操，
+ * 示例 3：
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
  */
 public class SearchRange {
 
-    public int[] solution(int[] nums, int target) {
+    /**
+     * 在排序数组中查找目标值的第一个和最后一个位置。
+     *
+     * @param nums   按非递减顺序排列的整数数组
+     * @param target 要查找的目标值
+     * @return 包含目标值在数组中起始位置和结束位置的数组。如果目标值不存在，返回 [-1, -1]
+     */
+    public static int[] searchRange(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
-            return new int[]{-1, -1};
+            return new int[]{-1, -1};// 数组为空，特殊处理
         }
         return new int[]{leftBound(nums, target), rightBound(nums, target)};
     }
 
-    int leftBound(int[] nums, int target) {
+    /**
+     * 使用二分查找找到目标值的第一个位置（左边界）。
+     *
+     * @param nums   按非递减顺序排列的整数数组
+     * @param target 要查找的目标值
+     * @return 目标值的第一个位置（索引），如果不存在则返回 -1
+     */
+    private static int leftBound(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+        // 二分查找左边界
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
+                left = mid + 1;//在右半部分查找
             } else {
-                // 收缩右边界
-                right = mid - 1;
+                right = mid - 1;//缩小到左半部分
             }
         }
-        // 检查left越界的情况
+        // 检查左边界是否越界
         if (left >= nums.length || nums[left] != target) {
             return -1;
         }
         return left;
     }
 
-    int rightBound(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
+    /**
+     * 使用二分查找找到目标值的最后一个位置（右边界）。
+     *
+     * @param nums   按非递减顺序排列的整数数组
+     * @param target 要查找的目标值
+     * @return 目标值的最后一个位置（索引），如果不存在则返回 -1
+     */
+    private static int rightBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        // 二分查找右边界
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] == target) {
-                // 收缩左边界
-                left = mid + 1;
+            if (nums[mid] > target) {
+                right = mid - 1;//在左半部分查找
+            } else {
+                left = mid + 1;//缩小到右半部分
             }
         }
-        // 检查right越界的情况
+        // 检查右边界是否越界
         if (right < 0 || nums[right] != target) {
             return -1;
         }
@@ -65,14 +87,8 @@ public class SearchRange {
     }
 
     public static void main(String[] args) {
-        SearchRange searchRange = new SearchRange();
-        int[] nums = new int[]{5, 7, 7, 8, 8, 10};
-        int target = 8;
-        SortUtil.print(searchRange.solution(nums, target));
-        target = 6;
-        SortUtil.print(searchRange.solution(nums, target));
-        nums = new int[]{};
-        target = 0;
-        SortUtil.print(searchRange.solution(nums, target));
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));//[3,4]
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));//[-1,-1]
+        System.out.println(Arrays.toString(searchRange(new int[]{}, 0)));//[-1,-1]
     }
 }
