55ff38b2d30c90d98f2aa1fbb43a1a0eb6cf5427
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-13 23:29:55 +0800

    跳跃游戏:<贪心>:maxReach;遍历{i>maxReach,false;maxReach=max(maxReach,i+nums[i]); maxReach>=n-1,true};false

--- src/main/java/org/example/greedy/array/JumpGame.java
+++ src/main/java/org/example/greedy/array/JumpGame.java
@@ -19,36 +19,36 @@ public class JumpGame {
 
     /**
      * 判断是否能够跳跃到最后一个下标
+     *
+     * @param nums 输入的数组，nums[i] 表示当前位置能跳跃的最大步数。
+     * @return 如果能到达最后一个下标，返回 true；否则返回 false。
      * 算法思路：
      * - 使用贪心算法，维护一个变量 maxReach，表示目前能跳到的最远位置。
      * - 遍历数组，更新 maxReach 并判断是否能到达最后一个下标。
      * - 如果 maxReach 在遍历过程中超过或等于数组的最后一个位置，则返回 true。
      * - 如果无法更新 maxReach，说明无法到达最后一个下标，返回 false。
-     *
-     * @param nums 输入的数组，nums[i] 表示当前位置能跳跃的最大步数。
-     * @return 如果能到达最后一个下标，返回 true；否则返回 false。
      */
     public static boolean canJump(int[] nums) {
-        // 初始化最大可达位置为0
+        // 初始化最远可达位置
         int maxReach = 0;
 
-        // 遍历数组的每个位置
+        // 遍历数组
         for (int i = 0; i < nums.length; i++) {
-            // 如果当前位置 i 超过了当前的最大可达位置，说明无法到达
+            // 当前位置不可达，返回false
             if (i > maxReach) {
                 return false;
             }
 
-            // 从当前位置 i 出发，更新最大可达位置
+            // 更新最大可达位置
             maxReach = Math.max(maxReach, i + nums[i]);
 
-            // 如果已经能够到达或超过最后一个下标，直接返回 true
+            // 如果最远位置超过或等于最后一个下标，返回true
             if (maxReach >= nums.length - 1) {
                 return true;
             }
         }
 
-        // 如果遍历完后仍无法到达最后一个下标，返回 false
+        // 遍历结束后，仍未能到达最后一个下标，返回false
         return false;
     }
 
