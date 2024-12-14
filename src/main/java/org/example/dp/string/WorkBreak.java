aff6e7f7e3a4d2b42df3a2ed25ae58eaa28dbee8
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-14 20:36:47 +0800

    单词拆分:<dp>:wordSet,dp[0]=true;dp[j]&&wordSet.contains(s.substring(j,i))? dp[i]=true

--- src/main/java/org/example/dp/string/WorkBreak.java
+++ src/main/java/org/example/dp/string/WorkBreak.java
@@ -27,46 +27,51 @@ public class WorkBreak {
 
     /**
      * 判断字符串 s 是否可以被拆分为字典中单词的组合。
-     * 核心思路：动态规划。
-     * 定义 dp[i] 表示字符串 s 从 0 到 i-1 的子串是否可以被拆分。
-     * 初始化 dp[0] 为 true，表示空字符串可以被拆分。
-     * 对于每一个位置 i，检查从 0 到 i 的子串是否包含字典中的单词，若是，则更新 dp[i]。
      *
      * @param s        待拆分的字符串
      * @param wordDict 单词字典列表
      * @return 字符串是否可以被拆分为字典中的单词
+     * 算法思路:
+     * 使用动态规划解决，定义一个布尔数组dp，其长度为s.length+1。
+     * dp[i]表示s[0...i-1]这个子串是否可以用字典中的单词拼接而成。
+     * dp[0] = true，表示空串可以被拆分。
+     * 遍历字符串，对于每个位置i，再从0到i的每个位置j切分，
+     * 如果dp[j]为true，且s[j...i-1]这个子串存在于字典中，则将dp[i]置于true。
+     * 若最终dp[s.length]为true，则说明整个字符串可以被字典单词拼出。
      */
     public static boolean wordBreak(String s, List<String> wordDict) {
-        // 将列表转为set以提高查找速度
-        Set<String> wordDictSet = new HashSet<>(wordDict);
-
-        // 动态规划数组，dp[i] 表示 s 的前 i 个字符是否可以被拆分
+        // 使用set加速字典查询
+        Set<String> wordSet = new HashSet<>(wordDict);
+        // dp数组，长度为s长度+1，用来表示子串能否由字典构成
         boolean[] dp = new boolean[s.length() + 1];
+        // 空串可以由字典构成(相当于不取任何单词)
+        dp[0] = true;
 
-        dp[0] = true; // 空串可以被拆分
+        System.out.println("初始化dp数组:" + Arrays.toString(dp));
 
-        // 遍历字符串的每个位置
+        // 外层循环i表示子串的结束位置(不包含)
+        // 即dp[i]表示s[0...i-1]可否被拆分
         for (int i = 1; i <= s.length(); i++) {
-
-            // 如果前j个字符可以被拆分，且j到i的子串在字典中
+            System.out.println("检查子串 s[0:" + (i - 1) + "] 是否可由字典拼出");
+            // 内层循环j用来分割子串s[0...i-1]为[0...j-1]和[j...i-1]
             for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
-                    dp[i] = true;// 更新 dp[i]
-                    System.out.println("Substring to index " + i + " is valid with words: " + s.substring(j, i));
-                    break;// 结束内层循环
+                // 如果dp[j]为true，说明[0...j-1]可由字典拼出
+                // 此时只要s[j...i-1]这部分也在字典中，那么dp[i]可设置为true
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    System.out.println("找到匹配：s[" + j + ":" + (i - 1) + "] = " + s.substring(j, i) + " 在字典中，且dp[" + j + "] = true");
+                    dp[i] = true;
+                    break;//已找到可行拆分，跳出内层循环
                 }
             }
+            System.out.println("dp[" + i + "] = " + dp[i]);
         }
-
-        // 返回整个字符串能否被拆分
         return dp[s.length()];
     }
 
     public static void main(String[] args) {
-        System.out.println("Can 'leetcode' be segmented? " + wordBreak("leetcode", Arrays.asList("leet", "code"))); // 输出: true
-
-        System.out.println("Can 'applepenapple' be segmented? " + wordBreak("applepenapple", Arrays.asList("apple", "pen"))); // 输出: true
-
-        System.out.println("Can 'catsandog' be segmented? " + wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat"))); // 输出: false
+        String s = "leetcode";
+        List<String> wordDict = Arrays.asList("leet", "code");
+        boolean result = wordBreak(s, wordDict);
+        System.out.println("是否可以由字典拼出：" + result);
     }
 }
