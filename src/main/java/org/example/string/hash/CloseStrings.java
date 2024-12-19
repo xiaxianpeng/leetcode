094e78745663c5f3b62f8c9d365ea1d3ec4be9c1
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-19 11:42:38 +0800

    确定两个字符串是否接近:<哈希>:count1,count2,exist1,exist2

--- src/main/java/org/example/string/hash/CloseStrings.java
+++ src/main/java/org/example/string/hash/CloseStrings.java
@@ -3,6 +3,31 @@ package org.example.string.hash;
 import java.util.Arrays;
 
 /**
+ * 1657. 确定两个字符串是否接近
+ * 如果可以使用以下操作从一个字符串得到另一个字符串，则认为两个字符串 接近 ：
+ * 操作 1：交换任意两个 现有 字符。
+ * 例如，abcde -> aecdb
+ * 操作 2：将一个 现有 字符的每次出现转换为另一个 现有 字符，并对另一个字符执行相同的操作。
+ * 例如，aacabb -> bbcbaa（所有 a 转化为 b ，而所有的 b 转换为 a ）
+ * 你可以根据需要对任意一个字符串多次使用这两种操作。
+ * 给你两个字符串，word1 和 word2 。如果 word1 和 word2 接近 ，就返回 true ；否则，返回 false 。
+ * 示例 1：
+ * 输入：word1 = "abc", word2 = "bca"
+ * 输出：true
+ * 解释：2 次操作从 word1 获得 word2 。
+ * 执行操作 1："abc" -> "acb"
+ * 执行操作 1："acb" -> "bca"
+ * 示例 2：
+ * 输入：word1 = "a", word2 = "aa"
+ * 输出：false
+ * 解释：不管执行多少次操作，都无法从 word1 得到 word2 ，反之亦然。
+ * 示例 3：
+ * 输入：word1 = "cabbba", word2 = "abbccc"
+ * 输出：true
+ * 解释：3 次操作从 word1 获得 word2 。
+ * 执行操作 1："cabbba" -> "caabbb"
+ * 执行操作 2："caabbb" -> "baaccc"
+ * 执行操作 2："baaccc" -> "abbccc"
  * Created on 2024/11/18 10:37
  */
 public class CloseStrings {
@@ -19,9 +44,11 @@ public class CloseStrings {
         if (word1.length() != word2.length()) {
             return false;
         }
+
         // 用数组记录每个字符的频次
         int[] count1 = new int[26];
         int[] count2 = new int[26];
+
         // 用数组记录每个字符串中的字符集合
         boolean[] exist1 = new boolean[26];
         boolean[] exist2 = new boolean[26];
