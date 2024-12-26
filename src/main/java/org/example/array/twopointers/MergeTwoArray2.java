1e37c406e4d10eb351a8fb84ff4239425275aa92
xiapeng <xiaxianpeng@autohome.com.cn>
2024-12-26 20:52:17 +0800

    合并两个有序数组:<双指针+从后向前>:i,j,k;从后向前比较合并避免覆盖

---
+++ src/main/java/org/example/array/twopointers/MergeTwoArray2.java
@@ -0,0 +1,46 @@
+package org.example.array.twopointers;
+
+import java.util.Arrays;
+
+/**
+ * 合并两个有序数组 nums1 和 nums2，返回一个新的合并数组。
+ */
+public class MergeTwoArray2 {
+
+    /**
+     * 合并两个有序数组 nums1 和 nums2，返回一个新的合并数组。
+     *
+     * @param nums1 第一个数组。
+     * @param nums2 第二个数组。
+     * @return 合并后的数组。
+     */
+    public static int[] mergeTwoArray(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[] ans = new int[nums1.length + nums2.length];
+        int p1 = 0;
+        int p2 = 0;
+        int cur;
+        while (p1 < len1 || p2 < len2) {
+            if (p1 == len1) { //转向nums2
+                cur = nums2[p2++];
+            } else if (p2 == len2) { //转向nums1
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) { // nums1[p1++]
+                cur = nums1[p1++];
+            } else { // nums2[p2++]
+                cur = nums2[p2++];
+            }
+            ans[p1 + p2 - 1] = cur;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {2, 5, 6};
+        int[] result = mergeTwoArray(nums1, nums2);
+        System.out.println(Arrays.toString(result));
+    }
+}
--- src/main/java/org/example/twopointers/array/MergeTwoArray.java
+++ src/main/java/org/example/twopointers/array/MergeTwoArray.java
@@ -38,72 +38,43 @@ public class MergeTwoArray {
      * @param n     第二个数组的有效元素个数。
      */
     public static void mergeTwoArray(int[] nums1, int m, int[] nums2, int n) {
-        // nums1 数组的最后有效元素索引
-        int num1Index = m - 1;
-        // nums2 数组的最后有效元素索引
-        int nums2Index = n - 1;
-        // nums1 数组的总大小的最后一个索引
-        int mergeIndex = m + n - 1;
+        // 指向 nums1 数组的最后有效元素索引
+        int i = m - 1;
+        // 指向 nums2 数组的最后有效元素索引
+        int j = n - 1;
+        // 指向 nums1 数组的最后一个索引
+        int k = m + n - 1;
 
         // 从后向前合并两个数组，确保不会覆盖 nums1 中的有效元素
-        while (num1Index >= 0 && nums2Index >= 0) {
-            if (nums1[num1Index] > nums2[nums2Index]) {
-                nums1[mergeIndex] = nums1[num1Index];
-                num1Index--;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
             } else {
-                nums1[mergeIndex] = nums2[nums2Index];
-                nums2Index--;
+                nums1[k] = nums2[j];
+                j--;
             }
-            mergeIndex--;
+            k--;
         }
 
         // 如果 nums2 中还有剩余元素，复制到 nums1 中
-        while (nums2Index >= 0) {
-            nums1[mergeIndex] = nums1[nums2Index];
-            nums2Index--;
-            mergeIndex--;
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 
-
-    /**
-     * 合并两个有序数组 nums1 和 nums2，返回一个新的合并数组。
-     *
-     * @param nums1 第一个数组。
-     * @param nums2 第二个数组。
-     * @return 合并后的数组。
-     */
-    public static int[] mergeTwoArray(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        int[] ans = new int[nums1.length + nums2.length];
-        int p1 = 0;
-        int p2 = 0;
-        int cur;
-        while (p1 < len1 || p2 < len2) {
-            if (p1 == len1) { //转向nums2
-                cur = nums2[p2++];
-            } else if (p2 == len2) { //转向nums1
-                cur = nums1[p1++];
-            } else if (nums1[p1] < nums2[p2]) { // nums1[p1++]
-                cur = nums1[p1++];
-            } else { // nums2[p2++]
-                cur = nums2[p2++];
-            }
-            ans[p1 + p2 - 1] = cur;
-        }
-        return ans;
-    }
 
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int[] nums2 = {2, 5, 6};
         mergeTwoArray(nums1, 3, nums2, 3);
-        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums1));// 输出: [1, 2, 3, 4, 5, 6]
 
-        int[] nums3 = {1, 2, 3};
-        int[] nums4 = {2, 5, 6};
-        int[] result = mergeTwoArray(nums3, nums4);
-        System.out.println(Arrays.toString(result));
+        int[] nums3 = {0, 0, 0};
+        int[] nums4 = {1, 2, 3};
+        mergeTwoArray(nums3, 0, nums4, 3);
+        System.out.println(Arrays.toString(nums3)); // 输出: [1, 2, 3]
     }
 }
