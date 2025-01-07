c3024ac33dd357f07be8f7cc1a5a638050d1f0e5
xiapeng <xiaxianpeng@autohome.com.cn>
2025-01-07 22:08:10 +0800

    查找和最小的K对数字:<最小堆>:PriorityQueue<int[]> minHeap;入堆;{出堆,(curr[0],curr[1]+1)入堆}

--- src/main/java/org/example/array/heap/KSmallestPairs.java
+++ src/main/java/org/example/array/heap/KSmallestPairs.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 /**
- * 373. 查找和最小的 K 对数字
+ * 373. 查找和最小的K对数字
  * 给定两个以 非递减顺序排列 的整数数组 nums1 和 nums2 , 以及一个整数 k 。
  * 定义一对值 (u,v)，其中第一个元素来自 nums1，第二个元素来自 nums2 。
  * 请找到和最小的 k 个数对 (u1,v1),  (u2,v2)  ...  (uk,vk) 。
