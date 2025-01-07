ecd679ffb3bcff50c8f870af44d1471a17509015
xiapeng <xiaxianpeng@autohome.com.cn>
2025-01-07 13:28:58 +0800

    二维区域和检索-矩阵不可变:<前缀和>:preSum[row][col]=当前元素+上方+左方-左上方；sumRegion=preSum[row2+1][col2+1]-preSum[row2-1][col1]-preSum[row1][col2+1]+preSum[row1][col1]

--- src/main/java/org/example/array/prefixsum/NumMatrix.java
+++ src/main/java/org/example/array/prefixsum/NumMatrix.java
@@ -10,10 +10,10 @@ package org.example.array.prefixsum;
  * 返回 左上角 (row1,col1)、右下角(row2,col2) 所描述的子矩阵的元素 总和 。
  * 输入
  * [[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]
- * 链接：https://leetcode-cn.com/problems/range-sum-query-2d-immutable
  * *********
  * *********
  * *********
+ * 链接：https://leetcode.cn/problems/range-sum-query-2d-immutable/solutions/2667331/tu-jie-yi-zhang-tu-miao-dong-er-wei-qian-84qp/
  * 算法步骤：
  * 当然，这个算法是基于二维前缀和技术用于快速计算二维矩阵中任意子矩阵的元素和。
  * 这里是算法的步骤和逻辑描述：
@@ -44,20 +44,35 @@ package org.example.array.prefixsum;
  * 这种方法在处理多个区域和查询时非常有效率。
  */
 public class NumMatrix {
+
     // 定义：preSum[i][j] 记录 matrix 中子矩阵 [0, 0, i-1, j-1] 的元素和
     private int[][] preSum;
 
+    /**
+     * 使用二维前缀和构造preSum数组
+     * preSum[i][j]表示原矩阵从(0,0)到(i-1,j-1)的元素和
+     *
+     * @param matrix 矩阵
+     */
     public NumMatrix(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        if (m == 0 || n == 0) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            preSum = new int[1][1];
             return;
         }
-        // 构造前缀和矩阵
-        preSum = new int[m + 1][n + 1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                // 递推计算每个矩阵 [0, 0, i-1, j-1] 的元素和
-                preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] + matrix[i - 1][j - 1] - preSum[i - 1][j - 1];
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        // 构造前缀和矩阵，比原矩阵多一行，多一列
+        preSum = new int[rows + 1][cols + 1];
+
+        // 填充preSum数组
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= cols; col++) {
+                // preSum[row][col] = 当前元素 + 上方 + 左方 - 左上方
+                preSum[row][col] = matrix[row - 1][col - 1]
+                        + preSum[row - 1][col]
+                        + preSum[row][col - 1]
+                        - preSum[row - 1][col - 1];
             }
         }
 
@@ -67,15 +82,17 @@ public class NumMatrix {
     /**
      * 计算左上角为 (row1, col1)、右下角为 (row2, col2) 的子矩阵的元素总和。
      *
-     * @param x1 子矩阵左上角的行索引
-     * @param y1 子矩阵左上角的列索引
-     * @param x2 子矩阵右下角的行索引
-     * @param y2 子矩阵右下角的列索引
+     * @param row1 子矩阵左上角的行索引
+     * @param col1 子矩阵左上角的列索引
+     * @param row2 子矩阵右下角的行索引
+     * @param col2 子矩阵右下角的列索引
      * @return 子矩阵的元素总和
      */
-    public int sumRegion(int x1, int y1, int x2, int y2) {
-        // 目标矩阵之和由四个相邻矩阵运算获得
-        return preSum[x2 + 1][y2 + 1] - preSum[x1][y2 + 1] - preSum[x2 + 1][y1] + preSum[x1][y1];
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return preSum[row2 + 1][col2 + 1] // (0,0)到(row2,col2)的所有元素的和
+                - preSum[row2 + 1][col1] // 减去左侧不需要的部分:(0,0)到(row2,col1-1)
+                - preSum[row1][col2 + 1] // 减去上方不需要的部分:(0,0)到(row1-1,col2)
+                + preSum[row1][col1]; // (0,0)到(row-1,col1-1)，因为两次减法减重了，所以加回来
     }
 
     public static void main(String[] args) {
