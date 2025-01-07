a89c11b4240b5bc1ec864b8d76e61d685f1351a9
xiapeng <xiaxianpeng@autohome.com.cn>
2025-01-07 10:38:06 +0800

    区域与检索-数组不可变:<前缀和>:preSum[i]=preSum[i-1]+nums[i-1];sunRange=preSum[right+1]-preSum[left]

--- src/main/java/org/example/array/prefixsum/NumArray.java
+++ src/main/java/org/example/array/prefixsum/NumArray.java
@@ -1,10 +1,24 @@
 package org.example.array.prefixsum;
 
 /**
- * @date 2021/03/04
- * @time 15:15
- * https://leetcode-cn.com/problems/range-sum-query-immutable/
- * 区域和检索 - 数组不可变
+ * 303. 区域和检索 - 数组不可变
+ * 给定一个整数数组  nums，处理以下类型的多个查询:
+ * 计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right
+ * 实现 NumArray 类：
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 中索引 left 和 right 之间的元素的 总和 ，
+ * 包含 left 和 right 两点（也就是 nums[left] + nums[left + 1] + ... + nums[right] )
+ * 示例 1：
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
  */
 public class NumArray {
 
@@ -16,14 +30,14 @@ public class NumArray {
     private int[] preSum;
 
     /**
-     * /* 输入一个数组，构造前缀和
+     * 输入一个数组，构造前缀和
      */
     public NumArray(int[] nums) {
         preSum = new int[nums.length + 1];
         // preSum[0] = 0, 为了方便计算累加和，不含任何元素的前缀和为0
         preSum[0] = 0;
         // 计算每个位置的前缀和
-        for (int i = 1; i < preSum.length; i++) {
+        for (int i = 1; i <= nums.length; i++) {
             preSum[i] = preSum[i - 1] + nums[i - 1];
         }
     }
