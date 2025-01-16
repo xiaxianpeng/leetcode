211ab07e19f9490cd508e4ccdae59b3a576d1c3e
xiapeng <xiaxianpeng@autohome.com.cn>
2025-01-16 10:30:47 +0800

    打印奇偶数:<semaphore>:print(currentSemaphore,nextSemaphore){curr.acquire;print;next.release}

--- src/main/java/org/example/multithread/PrintOddEven.java
+++ src/main/java/org/example/multithread/PrintOddEven.java
@@ -1,12 +1,14 @@
 package org.example.multithread;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * 打印奇偶数
  * Created on 2025/1/16 10:00
  */
 public class PrintOddEven {
 
-    public static class ParityPrinter {
+    private static class ParityPrinter {
         private final int max;
         private int count = 1;
         private final Object lock = new Object();
@@ -43,6 +45,42 @@ public class PrintOddEven {
         }
     }
 
+    private static class ParityPrinter2 {
+        private final int max;
+        private int count = 1;
+        // 初始为1，奇数线程先获取
+        private final Semaphore oddSemaphore = new Semaphore(1);
+        // 初始为0，偶数线程等待
+        private final Semaphore evenSemaphore = new Semaphore(0);
+
+        public ParityPrinter2(int max) {
+            this.max = max;
+        }
+
+        public void printOdd() {
+            print(oddSemaphore, evenSemaphore);
+        }
+
+        public void printEven() {
+            print(evenSemaphore, oddSemaphore);
+        }
+
+        private void print(Semaphore current, Semaphore next) {
+            // 每个线程只会打印一半的数
+            for (int i = 1; i <= max; i += 2) {
+                try {
+                    // 获取当前的信号量
+                    current.acquire();
+                    System.out.println(Thread.currentThread().getName() + " : " + count++);
+                    // 释放下一个信号量
+                    next.release();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // 打印 1-100
         ParityPrinter printer = new ParityPrinter(100);
@@ -51,5 +89,13 @@ public class PrintOddEven {
         Thread t2 = new Thread(printer::printEven, "Even");
         t1.start();
         t2.start();
+
+        // 打印 1-100
+        ParityPrinter printer2 = new ParityPrinter(100);
+        // 创建打印奇数和偶数的线程
+        Thread t3 = new Thread(printer2::printOdd, "Odd2");
+        Thread t4 = new Thread(printer2::printEven, "Even2");
+        t3.start();
+        t4.start();
     }
 }
