edae4d587acfb87e15b3963b738786bd055000a0
xiapeng <xiaxianpeng@autohome.com.cn>
2025-01-18 17:38:55 +0800

    找到字符串中所有字母异位词:<哈希+滑动窗口>:Map<Character,Integet> need,window;left,right,valid;startIndexs;

--- src/main/java/org/example/slidingwindow/string/FindAnagrams.java
+++ src/main/java/org/example/slidingwindow/string/FindAnagrams.java
@@ -23,20 +23,18 @@ import java.util.Map;
  * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
  * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
  * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
- * https://leetcode.cn/problems/find-all-anagrams-in-a-string/description/?envType=study-plan-v2&envId=labuladong-algorithm-note
  */
 public class FindAnagrams {
 
     /**
+     * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，
+     * 返回这些子串的起始索引
+     *
      * @param s 主字符串
      * @param p 字符串 p，其异位词子串为目标
      * @return 含有所有起始索引的列表
-     * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，
-     * 返回这些子串的起始索引
      */
     public static List<Integer> findAnagrams(String s, String p) {
-        // window 用来存储当前滑动窗口中各字符的实际出现次数。
-        Map<Character, Integer> window = new HashMap<>();
         // need 用来存储字符串 p 中每个字符的期望出现次数。
         Map<Character, Integer> need = new HashMap<>();
         // 初始化 need 哈希表，用于存储字符串 p 中每个字符及其出现的次数。
@@ -44,12 +42,15 @@ public class FindAnagrams {
             need.put(c, need.getOrDefault(c, 0) + 1);
         }
 
+        // window 用来存储当前滑动窗口中各字符的实际出现次数。
+        Map<Character, Integer> window = new HashMap<>();
+
         // left 和 right 分别代表滑动窗口的左右边界指针。
         int left = 0;
         int right = 0;
         // valid 变量用来记录窗口中满足 need 条件的字符数量。
         int valid = 0;
-        // ans 用来存储所有异位词的起始索引。
+        // anagramStarts 用来存储所有异位词的起始索引。
         List<Integer> anagramStarts = new ArrayList<>();
 
         // 开始遍历主字符串 s。
@@ -66,15 +67,18 @@ public class FindAnagrams {
                     valid++;
                 }
             }
-            // 当窗口大小等于 p 的长度时，开始收缩左边界。
-            while (right - left == p.length()) {
+
+            // 窗口大小 大于或等于 p 的长度时，开始收缩左边界。
+            while (right - left >= p.length()) {
                 // 如果窗口内的字符完全符合 need，记录当前 left 索引。
                 if (valid == need.size()) {
                     anagramStarts.add(left);
                 }
+
                 // d 是即将移出窗口的字符。
                 char d = s.charAt(left);
                 left++;// 缩小窗口。
+
                 // 进行窗口内数据的更新，移除字符 d 的出现次数。
                 if (need.containsKey(d)) {
                     if (window.get(d).equals(need.get(d))) {
@@ -83,8 +87,10 @@ public class FindAnagrams {
                     }
                     window.put(d, window.getOrDefault(d, 0) - 1);
                 }
+
             }
         }
+
         // 返回所有异位词的起始索引。
         return anagramStarts;
     }
