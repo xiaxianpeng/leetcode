34d3076336186640a2018730d4b4717c78e4e8a4
xiapeng <xiaxianpeng@autohome.com.cn>
2025-01-20 22:21:28 +0800

    矩阵置零:<哈希>:firstRowZero,firstColZero;标记;置零;处理首行首列

--- src/main/java/org/example/matrix/SetMatrixZeroes.java
+++ src/main/java/org/example/matrix/SetMatrixZeroes.java
@@ -1,25 +1,49 @@
-package org.example.array;
+package org.example.matrix;
 
 import java.util.Arrays;
 
 /**
  * 73. 矩阵置零
- * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+ * 给定一个 m x n 的矩阵，
+ * 如果一个元素为 0 ，
+ * 则将其所在行和列的所有元素都设为 0 。
+ * 请使用 原地 算法。
  * 示例 1：
- * 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
- * 输出：[[1,0,1],[0,0,0],[1,0,1]]
+ * 输入：matrix = [
+ * [1,1,1],
+ * [1,0,1],
+ * [1,1,1]
+ * ]
+ * 输出：[
+ * [1,0,1],
+ * [0,0,0],
+ * [1,0,1]]
  * 示例 2：
- * 输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
- * 输出：[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ * 输入：matrix = [
+ * [0,1,2,0],
+ * [3,4,5,2],
+ * [1,3,1,5]
+ * ]
+ * 输出：[
+ * [0,0,0,0],
+ * [0,4,5,0],
+ * [0,3,1,0]
+ * ]
  * Created on 2024/11/27 17:30
  */
 public class SetMatrixZeroes {
 
     /**
      * 使用原地算法将矩阵中的0所在的行和列置零。
-     * 思路：使用第一行和第一列作为标记，用于标识哪些行和列需要被置零。
      *
      * @param matrix 输入的二维矩阵
+     * @return void
+     * 算法思路：
+     * 1、首先判断第一行和第一列是否包含0，记录下来
+     * 2、使用第一行和第一列来标记需要置零的行和列
+     * 3、遍历矩阵的每个元素，如果元素为0，则标记其所在行列的第一个元素为0
+     * 4、根据标记，遍历矩阵将对应的行和列置为0
+     * 5、第一行和第一列要单独处理，因为他们同时用作标记位置
      */
     public static void setZeroes(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
@@ -28,6 +52,8 @@ public class SetMatrixZeroes {
 
         int m = matrix.length;
         int n = matrix[0].length;
+
+        // 判断第一行和第一列是否要置为0
         boolean firstRowZero = false;
         boolean firstColZero = false;
 
@@ -57,7 +83,7 @@ public class SetMatrixZeroes {
             }
         }
 
-        // 根据标记置零
+        // 从第二行和第二列开始，根据标记置零
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
